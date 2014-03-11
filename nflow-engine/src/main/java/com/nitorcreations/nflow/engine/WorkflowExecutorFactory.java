d3f83110b883276f037676e8ff87f1ad04e58d18
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2014-03-11 17:39:22 +0200

    remove unused logger

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
@@ -1,10 +1,7 @@
 package com.nitorcreations.nflow.engine;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.nitorcreations.nflow.engine.service.RepositoryService;
@@ -12,15 +9,13 @@ import com.nitorcreations.nflow.engine.service.RepositoryService;
 @Component
 public class WorkflowExecutorFactory {
 
-  private static final Logger logger = getLogger(WorkflowExecutorFactory.class);
-
   private final RepositoryService repository;
-  
+
   @Inject
   public WorkflowExecutorFactory(RepositoryService repository) {
     this.repository = repository;
   }
-   
+
   public WorkflowExecutor createExecutor(
       Integer instanceId) {
     return new WorkflowExecutor(instanceId, repository);
