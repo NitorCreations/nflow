2c28041e18dbd6964d6233bd010788ed6f6182f5
Juha Syrjälä <juha.syrjala@gmail.com>
2014-03-12 20:01:13 +0200

    Implement database connection pooling with BoneCP.

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/config/DatabaseConfiguration.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/config/DatabaseConfiguration.java
@@ -6,24 +6,27 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+import com.jolbox.bonecp.BoneCPDataSource;
 
 @Configuration
 public class DatabaseConfiguration {
 
   @Bean
   public DataSource datasource() {
-    SingleConnectionDataSource ds = new SingleConnectionDataSource();
-    ds.setDriverClassName("org.h2.Driver");
-    ds.setUrl(getProperty("db.url", "jdbc:h2:mem:test;TRACE_LEVEL_FILE=4"));
+    BoneCPDataSource ds = new BoneCPDataSource();
+    ds.setDriverClass("org.h2.Driver");
+    ds.setJdbcUrl(getProperty("db.url", "jdbc:h2:mem:test;TRACE_LEVEL_FILE=4"));
     ds.setUsername("sa");
     ds.setPassword("");
+    ds.setPartitionCount(4);
+    ds.setMaxConnectionsPerPartition(25);
     return ds;
   }
-  
+
   @Bean
   public DatabaseInitializer dbInitializer(DataSource ds) {
     return new DatabaseInitializer(ds);
   }
-  
+
 }
