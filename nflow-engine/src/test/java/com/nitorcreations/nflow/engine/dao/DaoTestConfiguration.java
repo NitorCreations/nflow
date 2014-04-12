991c131b6ec972ab72db6491251921d1e541852f
Esa Puttonen <esa.puttonen@nitorcreations.com>
2014-04-12 19:02:32 +0300

    Tune database configuration for JUnit tests

--- nflow-engine/src/test/java/com/nitorcreations/nflow/engine/dao/DaoTestConfiguration.java
+++ nflow-engine/src/test/java/com/nitorcreations/nflow/engine/dao/DaoTestConfiguration.java
@@ -1,35 +1,26 @@
 package com.nitorcreations.nflow.engine.dao;
 
-import static java.lang.System.getProperty;
-
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.nitorcreations.nflow.engine.config.DatabaseConfiguration;
 
 @Configuration
 @PropertySource({"classpath:junit.properties"})
 @EnableTransactionManagement
+@Import(DatabaseConfiguration.class)
 public class DaoTestConfiguration {
-	
+
   @Bean
-  public DataSource datasource() {
-    SingleConnectionDataSource ds = new SingleConnectionDataSource();
-    ds.setDriverClassName("org.h2.Driver");
-    ds.setUrl(getProperty("db.url", "jdbc:h2:mem:test"));
-    ds.setUsername("sa");
-    ds.setPassword("");
-    return ds;
+  public RepositoryDao repositoryDao(DataSource ds, Environment env) {
+    return new RepositoryDao(ds, env);
   }
-  
-  @Bean
-  public RepositoryDao repositoryDao(Environment env) {
-    return new RepositoryDao(datasource(), env);
-  }
-  
+
 }
 
