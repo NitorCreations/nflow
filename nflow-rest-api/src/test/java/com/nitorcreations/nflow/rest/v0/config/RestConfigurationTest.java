892875dc9ebe59abc709472f4b0fd8c36324b7c7
Esa Puttonen <esa.puttonen@nitorcreations.com>
2014-05-08 20:21:43 +0300

    Added dummy RestConfigurationTest

---
+++ nflow-rest-api/src/test/java/com/nitorcreations/nflow/rest/v0/config/RestConfigurationTest.java
@@ -0,0 +1,20 @@
+package com.nitorcreations.nflow.rest.v0.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.nitorcreations.nflow.rest.config.RestConfiguration;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestConfigurationTest {
+
+  @SuppressWarnings("unused")
+  private RestConfiguration configuration;
+
+  @Test
+  public void restConfigurationInstantiated() {
+    configuration = new RestConfiguration();
+  }
+
+}
