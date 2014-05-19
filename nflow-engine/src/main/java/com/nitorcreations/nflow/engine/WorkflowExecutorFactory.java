dcce69e0f97906e01507e8eac0c13785485c5948
Juha Syrjälä <juha.syrjala@gmail.com>
2014-05-19 21:42:15 +0300

    WorkflowExecutorFactory takes list of WorkflowExecutorListeners as parameters.

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
@@ -1,7 +1,12 @@
 package com.nitorcreations.nflow.engine;
 
+import static java.util.Collections.emptyList;
+
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.nitorcreations.nflow.engine.service.RepositoryService;
@@ -10,13 +15,20 @@ import com.nitorcreations.nflow.engine.service.RepositoryService;
 public class WorkflowExecutorFactory {
 
   private final RepositoryService repository;
+  private List<WorkflowExecutorListener> listeners = emptyList();
 
   @Inject
   public WorkflowExecutorFactory(RepositoryService repository) {
     this.repository = repository;
   }
 
+  @Autowired(required = false)
+  public WorkflowExecutorFactory setListeners(List<WorkflowExecutorListener> listeners) {
+    this.listeners = listeners;
+    return this;
+  }
+
   public WorkflowExecutor createExecutor(int instanceId) {
-    return new WorkflowExecutor(instanceId, repository);
+    return new WorkflowExecutor(instanceId, repository, listeners.toArray(new WorkflowExecutorListener[listeners.size()]));
   }
 }
