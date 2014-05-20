3ae9c5d19f11e67ee4c2355edf3755be163eac16
Isto Nikula <isto.nikula@nitorcreations.com>
2014-05-20 22:15:59 +0300

    Add missing finals to Sandman

--- nflow-engine/src/test/java/com/nitorcreations/nflow/engine/Sandman.java
+++ nflow-engine/src/test/java/com/nitorcreations/nflow/engine/Sandman.java
@@ -1,12 +1,12 @@
 package com.nitorcreations.nflow.engine;
 
 public class Sandman {
-  public static long SHORT_DELAY_MS = 50;
-  public static long SMALL_DELAY_MS = SHORT_DELAY_MS * 5;
-  public static long MEDIUM_DELAY_MS = SHORT_DELAY_MS * 10;
-  public static long LONG_DELAY_MS = SHORT_DELAY_MS * 200;
+  public static final long SHORT_DELAY_MS = 50;
+  public static final long SMALL_DELAY_MS = SHORT_DELAY_MS * 5;
+  public static final long MEDIUM_DELAY_MS = SHORT_DELAY_MS * 10;
+  public static final long LONG_DELAY_MS = SHORT_DELAY_MS * 200;
 
-  public static void sleep(long delayMs) {
+  public static final void sleep(long delayMs) {
     try {
       Thread.sleep(delayMs);
     } catch (InterruptedException e) {
