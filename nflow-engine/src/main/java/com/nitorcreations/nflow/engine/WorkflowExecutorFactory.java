8f23dca1434e5e600af8ad1a0a300db500307ebd
Mikko Tiihonen <mikko.tiihonen@nitorcreations.com>
2014-05-29 13:54:05 +0300

    Make listeners final, create the listeners array ahead of time. We know our WorkflowExecutor does not try to modify it

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/WorkflowExecutorFactory.java
@@ -1,12 +1,10 @@
 package com.nitorcreations.nflow.engine;
 
-import static java.util.Collections.emptyList;
-
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.nitorcreations.nflow.engine.service.RepositoryService;
@@ -17,21 +15,17 @@ public class WorkflowExecutorFactory {
 
   private final RepositoryService repository;
   private final ObjectStringMapper objectMapper;
-  private List<WorkflowExecutorListener> listeners = emptyList();
+  private final WorkflowExecutorListener[] listeners;
 
   @Inject
-  public WorkflowExecutorFactory(RepositoryService repository, ObjectStringMapper objectMapper) {
+  public WorkflowExecutorFactory(RepositoryService repository, ObjectStringMapper objectMapper, Provider<List<WorkflowExecutorListener>> listeners) {
     this.repository = repository;
     this.objectMapper = objectMapper;
-  }
-
-  @Autowired(required = false)
-  public WorkflowExecutorFactory setListeners(List<WorkflowExecutorListener> listeners) {
-    this.listeners = listeners;
-    return this;
+    List<WorkflowExecutorListener> l = listeners.get();
+    this.listeners = l != null ? l.toArray(new WorkflowExecutorListener[l.size()]) : new WorkflowExecutorListener[0];
   }
 
   public WorkflowExecutor createExecutor(int instanceId) {
-    return new WorkflowExecutor(instanceId, objectMapper, repository, listeners.toArray(new WorkflowExecutorListener[listeners.size()]));
+    return new WorkflowExecutor(instanceId, objectMapper, repository, listeners);
   }
 }
