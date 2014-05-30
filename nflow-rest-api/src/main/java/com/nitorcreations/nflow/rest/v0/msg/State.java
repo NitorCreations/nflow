3f7817bd98047d8caf554dcbb21f8e661794f1a0
Juha Syrjälä <juha.syrjala@gmail.com>
2014-05-30 11:12:25 +0300

    Add missing State.java

---
+++ nflow-rest-api/src/main/java/com/nitorcreations/nflow/rest/v0/msg/State.java
@@ -0,0 +1,36 @@
+package com.nitorcreations.nflow.rest.v0.msg;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "xxxion")
+public class State {
+
+  public State(String id, String type, String name, String description) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.description = description;
+  }
+
+  @ApiModelProperty(value = "State identifier", required=true)
+  public String id;
+
+  @ApiModelProperty(value = "State type", required=true)
+  public String type;
+
+  @ApiModelProperty(value = "State name", required=true)
+  public String name;
+
+  @ApiModelProperty(value = "State description", required=true)
+  public String description;
+
+  @ApiModelProperty(value = "Alternative transitions from this state", required=false)
+  public Set<String> transitions = new LinkedHashSet<>();
+
+  @ApiModelProperty(value = "Failure state for the this state", required=false)
+  public String onFailure;
+}
