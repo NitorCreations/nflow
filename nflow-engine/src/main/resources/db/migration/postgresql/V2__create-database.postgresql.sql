541f3c8b935fcad36650e0a1ab40caf3ceeb449d
Juha Syrjälä <juha.syrjala@gmail.com>
2014-06-18 23:26:39 +0300

    Add if exists to drop index statement for postgresql

--- nflow-engine/src/main/resources/db/migration/postgresql/V2__create-database.postgresql.sql
+++ nflow-engine/src/main/resources/db/migration/postgresql/V2__create-database.postgresql.sql
@@ -25,7 +25,7 @@ end;
 drop trigger if exists update_nflow_modified on nflow_workflow;
 create trigger update_nflow_modified before update on nflow_workflow for each row execute procedure update_modified();
 
-drop index nflow_workflow_activation;
+drop index if exists nflow_workflow_activation;
 create index nflow_workflow_activation on nflow_workflow(next_activation) where next_activation is not null;
 
 create table if not exists nflow_workflow_action (
