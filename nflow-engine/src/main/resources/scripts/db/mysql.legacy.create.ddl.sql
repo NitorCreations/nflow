dec5fa92536e11130d6c41dce4f6332bc5e3f35b
Mikko Tiihonen <gmokki@gmail.com>
2014-07-02 17:09:13 +0300

    Detect legact mysql versions and use separate ddl for them

--- nflow-engine/src/main/resources/scripts/db/mysql.legacy.create.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/mysql.legacy.create.ddl.sql
@@ -5,15 +5,20 @@ create table if not exists nflow_workflow (
   external_id varchar(64),
   state varchar(64) not null,
   state_text varchar(128),
-  next_activation timestamp(3) null,
+  next_activation timestamp null,
   executor_id int,
   retries int not null default 0,
-  created timestamp(3) not null default current_timestamp,
-  modified timestamp(3) not null default current_timestamp on update current_timestamp,
+  created timestamp not null,
+  modified timestamp not null default current_timestamp on update current_timestamp,
   executor_group varchar(64),
   constraint nflow_workflow_uniq unique (type, external_id),
   index nflow_workflow(next_activation)
 );
+
+drop trigger if exists nflow_workflow_insert;
+
+create trigger nflow_workflow_insert before insert on `nflow_workflow`
+  for each row set new.created = now();
 
 create table if not exists nflow_workflow_action (
   id int not null auto_increment primary key,
@@ -21,8 +26,8 @@ create table if not exists nflow_workflow_action (
   state varchar(64) not null,
   state_text varchar(128),
   retry_no int not null,
-  execution_start timestamp(3) not null,
-  execution_end timestamp(3) not null,
+  execution_start timestamp not null,
+  execution_end timestamp not null,
   foreign key (workflow_id) references nflow_workflow(id) on delete cascade
 );
 
@@ -40,7 +45,7 @@ create table if not exists nflow_executor (
   host varchar(64) not null,
   pid int not null,
   executor_group varchar(64),
-  started timestamp(3) not null default current_timestamp,
-  active timestamp(3),
-  expires timestamp(3)
+  started timestamp not null default current_timestamp,
+  active timestamp,
+  expires timestamp
 );
