2bbe0fdd48553ad555c01839a3a575798e4a4970
Juha Syrjälä <juha.syrjala@gmail.com>
2014-07-09 19:46:27 +0300

    Gather statistics for processing delay

--- nflow-metrics/src/test/java/com/nitorcreations/nflow/metrics/MetricsWorkflowExecutorListenerTest.java
+++ nflow-metrics/src/test/java/com/nitorcreations/nflow/metrics/MetricsWorkflowExecutorListenerTest.java
@@ -2,9 +2,11 @@ package com.nitorcreations.nflow.metrics;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -29,10 +31,14 @@ public class MetricsWorkflowExecutorListenerTest {
   MetricsWorkflowExecutorListener listener;
   WorkflowDefinition<?> definition = mock(WorkflowDefinition.class);
   WorkflowInstance instance = new WorkflowInstance.Builder().setRetries(2).setState("my-state").build();
+  WorkflowInstance instance2 = new WorkflowInstance.Builder().setRetries(2).setState("my-state")
+      .setNextActivation(DateTime.now()).build();
   StateExecution stateExecution = mock(StateExecution.class);
   ListenerContext context = new WorkflowExecutorListener.ListenerContext(
       definition, instance, stateExecution);
 
+  ListenerContext context2 = new WorkflowExecutorListener.ListenerContext(
+      definition, instance2, stateExecution);
   @Before
   public void setup() {
     ctx = new AnnotationConfigApplicationContext(Config.class);
@@ -46,6 +52,15 @@ public class MetricsWorkflowExecutorListenerTest {
     listener.beforeProcessing(context);
     assertEquals(1, metricRegistry.getHistograms().get("foobarName.myWorkflow.my-state.retries").getCount());
     assertNotNull(metricRegistry.getTimers().get("foobarName.myWorkflow.my-state.execution-time"));
+    assertNull(metricRegistry.getHistograms().get("foobarName.startup-delay"));
+  }
+
+  @Test
+  public void whenNextActivationIsSetBeforeContext() {
+    listener.beforeProcessing(context2);
+    assertEquals(1, metricRegistry.getHistograms().get("foobarName.myWorkflow.my-state.retries").getCount());
+    assertNotNull(metricRegistry.getTimers().get("foobarName.myWorkflow.my-state.execution-time"));
+    assertNotNull(metricRegistry.getHistograms().get("foobarName.startup-delay"));
   }
 
   @Test
