46db309716c57bbb2ebd2ee3a0e7078e93d39008
Mikko Tiihonen <gmokki@gmail.com>
2014-07-09 13:34:29 +0300

    Add logging for mysql version allow analysing the choice of mysql ddl. Use explicit bean name for database initializer

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/internal/storage/db/MysqlDatabaseConfiguration.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/internal/storage/db/MysqlDatabaseConfiguration.java
@@ -1,5 +1,7 @@
 package com.nitorcreations.nflow.engine.internal.storage.db;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
@@ -7,6 +9,7 @@ import java.sql.SQLException;
 import javax.inject.Named;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,16 +19,19 @@ import org.springframework.jdbc.datasource.DataSourceUtils;
 @Profile("nflow.db.mysql")
 @Configuration
 public class MysqlDatabaseConfiguration extends DatabaseConfiguration {
+  private static final Logger logger = getLogger(MysqlDatabaseConfiguration.class);
+
   public MysqlDatabaseConfiguration() {
     super("mysql");
   }
 
-  @Bean
+  @Bean(name="nflowDatabaseInitializer")
   @Override
   public DatabaseInitializer nflowDatabaseInitializer(@Named("nflow-datasource") DataSource dataSource, Environment env) {
     String dbType = "mysql";
     try (Connection c = DataSourceUtils.getConnection(dataSource)) {
       DatabaseMetaData meta = c.getMetaData();
+      logger.info("MySQL {}.{}, product version {}", meta.getDatabaseMajorVersion(), meta.getDatabaseMinorVersion(), meta.getDatabaseProductVersion());
       if (!meta.getDatabaseProductVersion().contains("MariaDB") && meta.getDatabaseMajorVersion() <=5 && meta.getDatabaseMinorVersion() <= 5) {
         dbType += ".legacy";
       }
