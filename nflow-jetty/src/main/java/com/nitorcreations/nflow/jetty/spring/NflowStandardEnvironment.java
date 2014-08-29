9f237b9fb2c2143e168e0002dfc3f01cae053fec
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2014-08-29 11:13:59 +0300

    rename local logger to avoid overriding field in super class

--- nflow-jetty/src/main/java/com/nitorcreations/nflow/jetty/spring/NflowStandardEnvironment.java
+++ nflow-jetty/src/main/java/com/nitorcreations/nflow/jetty/spring/NflowStandardEnvironment.java
@@ -12,7 +12,7 @@ import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.support.ResourcePropertySource;
 
 public class NflowStandardEnvironment extends StandardEnvironment {
-  private static final Logger logger = getLogger(NflowStandardEnvironment.class);
+  private static final Logger log = getLogger(NflowStandardEnvironment.class);
 
   public NflowStandardEnvironment(Map<String, Object> overrideProperties) {
     getPropertySources().addLast(new MapPropertySource("override", overrideProperties));
@@ -49,7 +49,7 @@ public class NflowStandardEnvironment extends StandardEnvironment {
     try {
       getPropertySources().addLast(new ResourcePropertySource(name, getClass().getClassLoader()));
     } catch (IOException e) {
-      logger.info("Failed to initialize environment-specific properties from resource {}", name);
+      log.info("Failed to initialize environment-specific properties from resource {}", name);
     }
   }
 }
