1a2ef9b86022f3901e22147f395a7b7516d9093a
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2014-08-29 13:57:53 +0300

    follow naming convention and ignore eclipse warning

--- nflow-jetty/src/main/java/com/nitorcreations/nflow/jetty/spring/NflowStandardEnvironment.java
+++ nflow-jetty/src/main/java/com/nitorcreations/nflow/jetty/spring/NflowStandardEnvironment.java
@@ -12,7 +12,8 @@ import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.support.ResourcePropertySource;
 
 public class NflowStandardEnvironment extends StandardEnvironment {
-  private static final Logger log = getLogger(NflowStandardEnvironment.class);
+  @SuppressWarnings("hiding")
+  private static final Logger logger = getLogger(NflowStandardEnvironment.class);
 
   public NflowStandardEnvironment(Map<String, Object> overrideProperties) {
     getPropertySources().addLast(new MapPropertySource("override", overrideProperties));
@@ -49,7 +50,7 @@ public class NflowStandardEnvironment extends StandardEnvironment {
     try {
       getPropertySources().addLast(new ResourcePropertySource(name, getClass().getClassLoader()));
     } catch (IOException e) {
-      log.info("Failed to initialize environment-specific properties from resource {}", name);
+      logger.info("Failed to initialize environment-specific properties from resource {}", name);
     }
   }
 }
