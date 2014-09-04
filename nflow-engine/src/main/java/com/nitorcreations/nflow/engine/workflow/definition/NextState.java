f9959567cabdc567c3d006b1d4574da4670d3b61
Juha Syrjälä <juha.syrjala@gmail.com>
2014-09-04 07:05:42 +0300

    Add some JavaDoc to NextState.

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/workflow/definition/NextState.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/workflow/definition/NextState.java
@@ -39,18 +39,31 @@ public class NextState {
     return isSaveTrace;
   }
 
+  /**
+   * Schedule retry for current state at time {@code activation}.
+   */
   public static NextState retryWithActivation(String reason, DateTime activation) {
     return new NextState(true, activation, null, reason);
   }
 
+  /**
+   * Schedule processing of state {@code nextState} at time {@code activation}.
+   */
   public static NextState moveToStateWithActivation(WorkflowState nextState, DateTime activation, String reason) {
     return new NextState(false, activation, nextState, reason);
   }
 
+  /**
+   * Schedule processing of state {@code nextState} immediately.
+   */
   public static NextState moveToState(WorkflowState nextState, String reason) {
     return moveToStateWithActivation(nextState, now(), reason);
   }
 
+  /**
+   * Set next state to {@finalState} and do not schedule its processing.
+   * Used to indicate final state of workflow or a manual state.
+   */
   public static NextState stopInState(WorkflowState finalState, String reason) {
     return moveToStateWithActivation(finalState, null, reason);
   }
