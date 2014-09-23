d4b733ae673a2f3a8f736860a246b9ae8f55b657
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2014-09-23 17:23:07 +0300

    improve javadoc and rename parameter

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/workflow/definition/NextAction.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/workflow/definition/NextAction.java
@@ -90,19 +90,20 @@ public class NextAction {
   /**
    * Set next state to {@code finalState} and do not schedule its
    * processing. The {@code finalState} workflow state type must
-   * be final. The state handler method of the state, even if it
-   * exists, is not executed. Additionally, there is no workflow
-   * action recorded for the {@code finalState}. If you want to
-   * execute the state handler method and record a workflow
-   * action, use one of the {@code moveToState} methods instead.
-   * @param state Final or manual workflow state.
+   * be final, e.g. it must be either end state or manual state.
+   * The state handler method of the state, even if it exists, is
+   * not executed. Additionally, there is no workflow action
+   * recorded for the {@code finalState}. If you want to execute
+   * the state handler method and record a workflow action, use
+   * one of the {@code moveToState} methods instead.
+   * @param state Final workflow state (end state or manual state).
    * @param reason The reason for the action.
    * @return A valid {@code NextAction} value.
    */
-  public static NextAction stopInState(WorkflowState state, String reason) {
-    assertNotNull(state, "State can not be null");
-    assertFinalState(state);
-    return new NextAction(null, state, reason);
+  public static NextAction stopInState(WorkflowState finalState, String reason) {
+    assertNotNull(finalState, "State can not be null");
+    assertFinalState(finalState);
+    return new NextAction(null, finalState, reason);
   }
 
   private static void assertFinalState(WorkflowState state) {
