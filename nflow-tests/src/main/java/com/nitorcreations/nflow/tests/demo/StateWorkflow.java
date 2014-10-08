e100bfe824357783f129cdede9af6c982c77710f
Juha Syrjälä <juha.syrjala@gmail.com>
2014-10-08 22:24:55 +0300

    Add integration test for state variables.

---
+++ nflow-tests/src/main/java/com/nitorcreations/nflow/tests/demo/StateWorkflow.java
@@ -0,0 +1,107 @@
+package com.nitorcreations.nflow.tests.demo;
+
+import static com.nitorcreations.nflow.engine.workflow.definition.NextAction.moveToState;
+import static com.nitorcreations.nflow.engine.workflow.definition.WorkflowStateType.end;
+import static com.nitorcreations.nflow.engine.workflow.definition.WorkflowStateType.manual;
+import static com.nitorcreations.nflow.engine.workflow.definition.WorkflowStateType.normal;
+import static com.nitorcreations.nflow.engine.workflow.definition.WorkflowStateType.start;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.done;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.error;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.state1;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.state2;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.state3;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.state4;
+import static com.nitorcreations.nflow.tests.demo.StateWorkflow.State.state5;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+
+import com.nitorcreations.nflow.engine.workflow.definition.NextAction;
+import com.nitorcreations.nflow.engine.workflow.definition.StateExecution;
+import com.nitorcreations.nflow.engine.workflow.definition.StateVar;
+import com.nitorcreations.nflow.engine.workflow.definition.WorkflowDefinition;
+import com.nitorcreations.nflow.engine.workflow.definition.WorkflowSettings;
+import com.nitorcreations.nflow.engine.workflow.definition.WorkflowStateType;
+
+public class StateWorkflow extends WorkflowDefinition<StateWorkflow.State> {
+
+  public static enum State implements com.nitorcreations.nflow.engine.workflow.definition.WorkflowState {
+    state1(start, "Set variable 1"),
+    state2(normal, "Set variable 2"),
+    state3(normal, "Update variable 2"),
+    state4(normal, "Do nothing"),
+    state5(normal, "Update variable 2"),
+    done(end, "Finished"),
+    error(manual, "Error state");
+
+    private final WorkflowStateType type;
+    private final String description;
+
+    private State(WorkflowStateType type, String description) {
+      this.type = type;
+      this.description = description;
+    }
+
+    @Override
+    public WorkflowStateType getType() {
+      return type;
+    }
+
+    @Override
+    public String getName() {
+      return name();
+    }
+
+    @Override
+    public String getDescription() {
+      return description;
+    }
+  }
+
+  public StateWorkflow() {
+    super("stateWorkflow", state1, error, new WorkflowSettings.Builder().setMinErrorTransitionDelay(0)
+        .setMaxErrorTransitionDelay(0).setShortTransitionDelay(0).setMaxRetries(3).build());
+    permit(state1, state2);
+    permit(state2, state3);
+    permit(state3, state4);
+    permit(state4, state5);
+    permit(state5, done);
+  }
+
+  public NextAction state1(StateExecution execution, @StateVar(value = "variable1", instantiateIfNotExists = true) Variable variable1) {
+    variable1.value = "foo1";
+    return moveToState(state2, "variable1 is set to " + variable1.value);
+  }
+
+  public NextAction state2(StateExecution execution, @StateVar(value = "variable2", instantiateIfNotExists = true) Variable variable2) {
+    variable2.value = "bar1";
+    return moveToState(state3, "variable1 is set to " + variable2.value);
+  }
+
+  public NextAction state3(StateExecution execution, @StateVar(value = "variable2") Variable variable2) {
+    variable2.value = "bar2";
+    return moveToState(state4, "variable2 is set to " + variable2.value);
+  }
+
+  public NextAction state4(StateExecution execution, @StateVar(value = "variable1") Variable variable1,
+      @StateVar(value = "variable2") Variable variable2) {
+    return moveToState(state5, format("variable1=%s variable2=%s", variable1, variable2));
+  }
+
+  public NextAction state5(StateExecution execution, @StateVar(value = "variable2") Variable variable2) {
+    variable2.value = "bar3";
+    return moveToState(done, "variable2 is set to " + variable2.value);
+  }
+
+  public void done(StateExecution execution) {
+  }
+
+  public static class Variable {
+    public String value;
+
+    @Override
+    public String toString() {
+      return reflectionToString(this);
+    }
+  }
+
+}
---
+++ nflow-tests/src/test/java/com/nitorcreations/nflow/tests/StateVariablesTest.java
@@ -0,0 +1,83 @@
+package com.nitorcreations.nflow.tests;
+
+import static java.util.Collections.singletonMap;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.runners.MethodSorters.NAME_ASCENDING;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.ClassRule;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import com.nitorcreations.nflow.rest.v1.msg.Action;
+import com.nitorcreations.nflow.rest.v1.msg.CreateWorkflowInstanceRequest;
+import com.nitorcreations.nflow.rest.v1.msg.CreateWorkflowInstanceResponse;
+import com.nitorcreations.nflow.rest.v1.msg.ListWorkflowInstanceResponse;
+import com.nitorcreations.nflow.tests.demo.StateWorkflow;
+import com.nitorcreations.nflow.tests.demo.StateWorkflow.State;
+import com.nitorcreations.nflow.tests.runner.NflowServerRule;
+
+@FixMethodOrder(NAME_ASCENDING)
+public class StateVariablesTest extends AbstractNflowTest {
+  @ClassRule
+  public static NflowServerRule server = new NflowServerRule.Builder().build();
+  private static CreateWorkflowInstanceRequest createRequest;
+  private static CreateWorkflowInstanceResponse createResponse;
+
+  public StateVariablesTest() {
+    super(server);
+  }
+
+  @Test(timeout = 5000)
+  public void t01_createStateWorkflow() {
+    createRequest = new CreateWorkflowInstanceRequest();
+    createRequest.type = "stateWorkflow";
+    createRequest.externalId = UUID.randomUUID().toString();
+    createResponse = createWorkflowInstance(createRequest);
+    assertThat(createResponse.id, notNullValue());
+  }
+
+  @Test(timeout = 5000)
+  public void t02_checkStateVariables() throws InterruptedException {
+    ListWorkflowInstanceResponse listResponse;
+    do {
+      listResponse = getWorkflowInstance(createResponse.id, "done");
+    } while (listResponse.nextActivation != null);
+    assertEquals(2, listResponse.stateVariables.size());
+    assertEquals(singletonMap("value", "foo1"), listResponse.stateVariables.get("variable1"));
+    assertEquals(singletonMap("value", "bar3"), listResponse.stateVariables.get("variable2"));
+
+    assertEquals(6, listResponse.actions.size());
+    assertState(listResponse.actions, 0, StateWorkflow.State.state1, "foo1", null);
+    assertState(listResponse.actions, 1, StateWorkflow.State.state2, null, "bar1");
+    assertState(listResponse.actions, 2, StateWorkflow.State.state3, null, "bar2");
+    assertState(listResponse.actions, 3, StateWorkflow.State.state4, null, null);
+    assertState(listResponse.actions, 4, StateWorkflow.State.state5, null, "bar3");
+    assertState(listResponse.actions, 5, StateWorkflow.State.done, null, null);
+  }
+
+  private void assertState(List<Action> actions, int index, State state, String variable1, String variable2) {
+    Action action = actions.get(index);
+    assertEquals(state.name(), action.state);
+    assertVariable(action, "variable1", variable1);
+    assertVariable(action, "variable2", variable2);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void assertVariable(Action action, String key, String expectedValue) {
+    Map<String, String> value = null;
+    if (action.updatedStateVariables != null) {
+      value = (Map<String, String>) action.updatedStateVariables.get(key);
+    }
+
+    if (value == null && expectedValue == null) {
+      return;
+    }
+    assertEquals(expectedValue, value.get("value"));
+  }
+}
