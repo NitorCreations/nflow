4eb669cf0834c1d0d015f3d9f6a844adf351da6e
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2014-10-09 15:31:09 +0300

    inject jdbctemplate instead of datasource

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/internal/dao/WorkflowInstanceDao.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/internal/dao/WorkflowInstanceDao.java
@@ -2,6 +2,8 @@ package com.nitorcreations.nflow.engine.internal.dao;
 
 import static com.nitorcreations.nflow.engine.internal.dao.DaoUtil.toDateTime;
 import static com.nitorcreations.nflow.engine.internal.dao.DaoUtil.toTimestamp;
+import static com.nitorcreations.nflow.engine.internal.storage.db.DatabaseConfiguration.NFLOW_JDBC;
+import static com.nitorcreations.nflow.engine.internal.storage.db.DatabaseConfiguration.NFLOW_NAMED_JDBC;
 import static org.apache.commons.lang3.StringUtils.left;
 import static org.springframework.util.CollectionUtils.isEmpty;
 import static org.springframework.util.StringUtils.collectionToDelimitedString;
@@ -20,7 +22,6 @@ import java.util.Map.Entry;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.sql.DataSource;
 
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.springframework.dao.DataAccessException;
@@ -58,14 +59,18 @@ public class WorkflowInstanceDao {
   ExecutorDao executorInfo;
 
   /**
-   * Use setter injection because having the dataSource in constructor may not work
+   * Use setter injection because constructor injection may not work
    * when nFlow is used in some legacy systems.
-   * @param dataSource The nFlow data source.
+   * @param jdbcTemplate The JDBC template for accessing the nFlow data source.
    */
   @Inject
-  public void setDataSource(@Named("nflowDatasource") DataSource dataSource) {
-    this.jdbc = new JdbcTemplate(dataSource);
-    this.namedJdbc = new NamedParameterJdbcTemplate(dataSource);
+  public void setJdbcTemplate(@Named(NFLOW_JDBC) JdbcTemplate jdbcTemplate) {
+    this.jdbc = jdbcTemplate;
+  }
+
+  @Inject
+  public void setNamedParameterJdbcTemplate(@Named(NFLOW_NAMED_JDBC) NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    this.namedJdbc = namedParameterJdbcTemplate;
   }
 
   @Inject
