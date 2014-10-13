2178aa2f07418c8bd59792e950cb41a82c680f61
Juha Syrjälä <juha.syrjala@gmail.com>
2014-10-14 00:17:24 +0300

    workflow page fetches instance and related definition.

--- app/scripts/controllers/workflow.js
+++ app/scripts/controllers/workflow.js
@@ -7,14 +7,24 @@ var app = angular.module('nflowVisApp');
 
 app.factory('Workflows', function ($resource, config) {
   return $resource(config.nflowUrl + '/v1/workflow-instance/:id',
-                   {id: '@id', include: 'actions'},
+                   {id: '@id', include: 'actions,currentStateVariables,actionStateVariables'},
                    {'update': {method: 'PUT'},
                    });
 });
 
-app.controller('WorkflowCtrl', function ($scope, Workflows, $routeParams) {
+app.controller('WorkflowCtrl', function ($scope, Workflows, WorkflowDefinitions, $routeParams) {
     Workflows.get({id: $routeParams.id},
-                          function(data) {
-                            console.debug(data);
+                          function(workflow) {
+                            $scope.workflow = workflow;
+                            console.debug(workflow);
+
+                            WorkflowDefinitions.get({type: workflow.type},
+                                                   function(data) {
+                                                     $scope.definition = _.first(data);
+                                                     console.debug($scope.definition);
+                                                   });
+
                           });
+
+
 });
