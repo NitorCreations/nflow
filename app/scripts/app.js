a3a391c7f1bd915a4022652fe61c4494d1a65166
Juha Syrjälä <juha.syrjala@gmail.com>
2014-10-17 17:25:10 +0300

    reverse filter returns empty array for null

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -45,6 +45,9 @@ angular
   })
   .filter('reverse', function() {
     return function(items) {
+      if(!items) {
+        return [];
+      }
       return items.slice().reverse();
     };
   });
