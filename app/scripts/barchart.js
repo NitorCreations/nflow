039fe6fd23d95e556be293e2a13b121323a5ce92
Juha Syrjälä <juha.syrjala@gmail.com>
2014-10-31 21:32:15 +0200

    Add tool tips to bars

--- app/scripts/barchart.js
+++ app/scripts/barchart.js
@@ -37,35 +37,40 @@ function drawStateExecutionGraph(canvasId, stats) {
     },
   };
   */
+  function execTypeName(name) {
+    if(name === 'nonScheduled') {
+      return 'Passive';
+    }
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
 
-
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  var margin = {top: 20, right: 120, bottom: 130, left: 40},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
   var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], 0.1);
+    .rangeRoundBands([0, width], 0.1);
 
   var y = d3.scale.linear()
-      .rangeRound([height, 0]);
+    .rangeRound([height, 0]);
 
   var color = d3.scale.ordinal()
-      .range(['#98abc5',  '#7b6888',  '#a05d56', '#ff8c00']);
+    .range(['#98abc5',  '#7b6888',  '#a05d56', '#ff8c00']);
 
   var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient('bottom');
+    .scale(x)
+    .orient('bottom');
 
   var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient('left')
-      .tickFormat(d3.format('.2s'));
+    .scale(y)
+    .orient('left')
+    .tickFormat(d3.format('.2s'));
 
   var svg = d3.select('#' + canvasId)
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
     .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   var states = _.map(stats, function (state, stateName) {
     var r = _.merge({}, state);
@@ -86,21 +91,31 @@ function drawStateExecutionGraph(canvasId, stats) {
   x.domain(stateNames);
   y.domain([0, d3.max(states, function(d) { return d._total; })]);
 
+  // X axis
   svg.append('g')
     .attr('class', 'x axis')
     .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
+    .call(xAxis)
+    .selectAll('text')
+      .style('text-anchor', 'start')
+      //.attr('dx', '-.8em')
+      //.attr('dy', '.15em')
+      .attr('transform', 'rotate(20)');
 
+  // Y axis
   svg.append('g')
     .attr('class', 'y axis')
     .call(yAxis)
-    .append('text')
+    .append('text');
+  /*
     .attr('transform', 'rotate(-90)')
     .attr('y', 6)
-    .attr('dy', '.71em')
+    .attr('dy', '.35em')
     .style('text-anchor', 'end')
     .text('State Execution statistics');
+*/
 
+  // Stacked bars
   var state = svg.selectAll('.state')
     .data(states)
     .enter().append('g')
@@ -113,25 +128,30 @@ function drawStateExecutionGraph(canvasId, stats) {
     .attr('width', x.rangeBand())
     .attr('y', function(d) { return y(d.y1); })
     .attr('height', function(d) { return y(d.y0) - y(d.y1); })
-    .style('fill', function(d) { return color(d.name); });
+    .style('fill', function(d) { return color(d.name); })
+    .append('title').text(function(d) { return execTypeName(d.name) + ' ' + (  y(d.y0) - y(d.y1) )})
 
+  // Legend
   var legend = svg.selectAll('.legend')
     .data(color.domain().slice().reverse())
     .enter().append('g')
     .attr('class', 'legend')
     .attr('transform', function(d, i) { return 'translate(0,' + i * 20 + ')'; });
 
+  // Legend boxes
   legend.append('rect')
-    .attr('x', width - 18)
+    .attr('x', width)
     .attr('width', 18)
     .attr('height', 18)
     .style('fill', color);
 
+  // Legend texts
   legend.append('text')
-    .attr('x', width - 24)
+    .attr('x', width - 6)
     .attr('y', 9)
     .attr('dy', '.35em')
-    .style('text-anchor', 'end')
-    .text(function(d) { return d; });
+    .attr('dx', '2em')
+    .style('text-anchor', 'start')
+    .text(execTypeName);
 
 }
