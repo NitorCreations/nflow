06c6e7e428aafe4de946267624d33a7def3536cf
Juha Syrjälä <juha.syrjala@gmail.com>
2014-10-31 14:12:51 +0200

    Add missing NotFoundExceptionMapper

---
+++ nflow-rest-api/src/main/java/com/nitorcreations/nflow/rest/config/NotFoundExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.nitorcreations.nflow.rest.config;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+  @Override
+  public Response toResponse(NotFoundException e) {
+    return Response.status(Response.Status.NOT_FOUND).build();
+  }
+
+}
