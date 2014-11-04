c2e4ffbe48efac75b8da91ec54384ffd00b8a3fd
Juha Syrjälä <juha.syrjala@gmail.com>
2014-11-04 19:33:36 +0200

    Add executors table to front page. Table refreshes periodically

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var app = angular.module('nflowVisApp');
+app.factory('Executors', function($resource, config) {
+  return $resource(config.nflowUrl + '/v1/workflow-executor');
+});
+
 /**
  * @ngdoc function
  * @name nflowVisApp.controller:MainCtrl
@@ -9,7 +14,20 @@
  */
 angular.module('nflowVisApp')
 .constant('config', new Config())
-.controller('MainCtrl', function ($scope, WorkflowDefinitions) {
+.controller('MainCtrl', function ($scope, $rootScope, $interval, WorkflowDefinitions, Executors) {
   $scope.workflows = WorkflowDefinitions.query();
+  $scope.executors = Executors.query();
+  function updateExecutors() {
+    Executors.query(function(executors) {
+      console.info("Fetch executors");
+      $scope.executors = executors;
+    });
+  }
+
+  if(!$rootScope.executorQueryRunning) {
+    $rootScope.executorQueryRunning = true;
+    $interval(updateExecutors, 15*1000);
+  }
+
 })
 ;
