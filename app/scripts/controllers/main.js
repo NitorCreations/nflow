2be635f42be2d993e5fec1d42774814ae98d8f49
Juha Syrjälä <juha.syrjala@gmail.com>
2014-11-16 12:01:59 +0200

    Color code executor statuses.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,17 @@ angular.module('nflowVisApp')
 .controller('MainCtrl', function MainCtrl($scope, $rootScope,
                                            WorkflowDefinitions, ExecutorPoller) {
   $scope.workflows = WorkflowDefinitions.query();
+
+  $scope.executorClass = function(executor) {
+    var expires = moment(executor.expires);
+    var active = moment(executor.active);
+    if(active.add(1, 'days').isBefore(new Date())) {
+      return;
+    }
+    if(expires.isBefore(new Date())) {
+      return 'warning';
+    }
+    return 'success';
+  };
 });
 
