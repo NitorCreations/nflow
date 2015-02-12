a530344e77acb74bdf29e42d97ea1e957f412b4d
Isto Nikula <isto.nikula@iki.fi>
2015-02-12 23:39:12 +0200

    componentize search page: refactor SearchResultCtrl
    Define workflow state types in constants module.

--- src/app/search/searchResult.js
+++ src/app/search/searchResult.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var m = angular.module('nflowVisApp.search.searchResult', [
+    'nflowVisApp.contants',
     'nflowVisApp.filters'
   ]);
 
@@ -19,41 +20,28 @@
     };
   });
 
-  m.controller('SearchResultCtrl', function() {
+  m.controller('SearchResultCtrl', function(WorkflowStateType) {
     var self = this;
     self.getStateClass = getStateClass;
 
+    var classByStateType = {};
+    classByStateType[WorkflowStateType.NORMAL] = 'info';
+    classByStateType[WorkflowStateType.MANUAL] = 'warning';
+    classByStateType[WorkflowStateType.END] = 'success';
+    classByStateType[WorkflowStateType.ERROR] = 'danger';
+
     function getStateClass(result) {
-      if (!result) {
-        return '';
-      }
-      var def = getDefinition(result.type);
-      if (!def) {
-        return '';
-      }
-      var state = _.find(def.states, function (s) {
-        return s.id === result.state;
-      });
-      if (state.id === def.onError) {
-        return 'danger';
-      }
-      if (state.type === 'normal') {
-        return 'info';
-      } else if (state.type === 'manual') {
-        return 'warning';
-      } else if (state.type === 'end') {
-        return 'success';
-      } else if (state.type === 'error') {
-        return 'danger';
+      if (!result) { return ''; }
+
+      var d = _.find(self.definitions, function (d) { return d.type === result.type; });
+      if (d) {
+        var state = _.find(d.states, function (s) { return s.id === result.state; });
+        if (state) {
+          if (state.id === d.onError) { return 'danger'; }
+          return classByStateType[state.type] || '';
+        }
       }
       return '';
-    }
-
-
-    function getDefinition(type) {
-      return _.find(self.definitions, function (def) {
-        return def.type === type;
-      });
     }
 
   });
