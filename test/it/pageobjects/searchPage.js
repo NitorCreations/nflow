594559510e9da87b9d37216d447d3b5a351546de
Isto Nikula <isto.nikula@iki.fi>
2015-03-02 11:06:43 +0200

    smoketest: test search page

--- test/it/pageobjects/searchPage.js
+++ test/it/pageobjects/searchPage.js
@@ -12,11 +12,37 @@ module.exports = function (spec) {
   spec.externalId = element(by.model('ctrl.model.externalId'));
   spec.resultRows = $$('table#search-result tbody tr');
 
+  that.get = function(type, state) {
+    var url = '/#/search/';
+
+    var t = 'type=' + type;
+    var s = 'state=' + state;
+
+    if (type && state) {
+      url += '?' + t + '&' + s;
+    } else if (type) {
+      url += '?' + t;
+    } else if (state) {
+      url += '?' + s;
+    }
+
+    browser.get(url);
+    expect(that.isDisplayed()).toBeTruthy();
+  };
+
   that.getType = function() { return spec.getValue(spec.type); };
   that.getState = function() { return spec.getValue(spec.state); };
   that.getInstanceId = function() { return spec.getValue(spec.instanceId); };
   that.getBusinessKey = function() { return spec.getValue(spec.businessKey); };
   that.getExternalId = function() { return spec.getValue(spec.externalId); };
+
+  that.setType = function(type) {
+    spec.type.sendKeys(type);
+  };
+
+  that.getStateOptions = function() {
+    return spec.state.$$('option').getText();
+  };
 
   that.hasResults = function() {
     return spec.resultRows.count().then(function(count){ return count > 0; });
