f01ec7bcfc2aa195d20cf90b68b37bc16f5a5bf2
Isto Nikula <isto.nikula@iki.fi>
2015-03-10 13:58:50 +0200

    build: add ng-strict-di to dist build v2

--- Gruntfile.js
+++ Gruntfile.js
@@ -444,12 +444,12 @@ module.exports = function (grunt) {
     'ngAnnotate',
     'copy:dist',
     'targethtml:dist',
-    //'cdnify',
-    //'cssmin',
-    //'uglify',
-    //'filerev',
-    //'usemin',
-    //'htmlmin'
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin',
+    'htmlmin'
   ]);
 
   grunt.registerTask('default', [
