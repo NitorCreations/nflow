d092f99083f0e3f003a937e4fd7294db671f164f
Juha Syrjälä <juha.syrjala@gmail.com>
2015-03-31 20:47:22 +0300

    Executors table shows only executors that have been active in last 24h

--- src/app/front-page/executorTable.js
+++ src/app/front-page/executorTable.js
@@ -18,9 +18,23 @@
     };
   });
 
-  m.controller('ExecutorTableCtrl', function () {
+  m.controller('ExecutorTableCtrl', function (Time) {
     var self = this;
     self.executorClass = executorClass;
+    self.isActive = isActive;
+
+    function isActive(executor) {
+      var passiveLimit = Time.currentMoment().add(-24, 'hours');
+      if(executor.expires) {
+        var expires = moment(executor.expires);
+        if(expires.isBefore(passiveLimit)) {
+          return false;
+        }
+      } else {
+        return moment(executor.started).isAfter(passiveLimit);
+      }
+      return true;
+    }
 
     function executorClass(executor, now) {
       now = now || moment();
