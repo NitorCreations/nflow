9473951dfa67aa466e2bbe62cda3276afda75188
Juha Syrjälä <juha.syrjala@gmail.com>
2015-05-15 20:08:22 +0300

    Add test for StatisticsResource.queryStatistics

--- nflow-rest-api/src/test/java/com/nitorcreations/nflow/rest/v1/StatisticsResourceTest.java
+++ nflow-rest-api/src/test/java/com/nitorcreations/nflow/rest/v1/StatisticsResourceTest.java
@@ -2,12 +2,16 @@ package com.nitorcreations.nflow.rest.v1;
 
 import static java.util.Collections.emptyMap;
 import static org.hamcrest.Matchers.is;
+import static org.joda.time.DateTime.now;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
 
+import com.nitorcreations.nflow.engine.workflow.statistics.Statistics;
+import com.nitorcreations.nflow.rest.v1.msg.StatisticsResponse;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,19 +33,39 @@ public class StatisticsResourceTest {
   private StatisticsService service;
   @Mock
   private StatisticsConverter converter;
+  @Mock
+  StatisticsResponse expected;
+  @Mock
+  Statistics stats;
+
+  DateTime createdAfter = now(), createdBefore = now().plusMinutes(1), modifiedAfter = now().plusMinutes(2),
+          modifiedBefore = now().plusMinutes(3);
 
   @Before
   public void setup() {
-    Map<String, Map<String, WorkflowDefinitionStatistics>> stats = emptyMap();
-    when(service.getWorkflowDefinitionStatistics("dummy", null, null, null, null)).thenReturn(stats);
-    when(converter.convert(stats)).thenReturn(new WorkflowDefinitionStatisticsResponse());
+
   }
 
   @Test
-  public void getWorkflowDefinitionStatistics() {
-    WorkflowDefinitionStatisticsResponse statistics = resource.getStatistics("dummy", null, null, null, null);
+  public void queryStatisticsDelegatesToStatisticsService() {
+    when(service.queryStatistics()).thenReturn(stats);
+    when(converter.convert(stats)).thenReturn(expected);
+    StatisticsResponse response = resource.queryStatistics();
 
-    verify(service).getWorkflowDefinitionStatistics("dummy", null, null, null, null);
+    verify(service).queryStatistics();
+    assertThat(response, is(response));
+  }
+
+  @Test
+  public void getWorkflowDefinitionStatisticsDelegatesToStatisticsService() {
+    Map<String, Map<String, WorkflowDefinitionStatistics>> stats = emptyMap();
+    when(service.getWorkflowDefinitionStatistics("dummy", null, null, null, null)).thenReturn(stats);
+    when(converter.convert(stats)).thenReturn(new WorkflowDefinitionStatisticsResponse());
+
+    WorkflowDefinitionStatisticsResponse statistics = resource.getStatistics("dummy", createdAfter, createdBefore,
+            modifiedAfter, modifiedBefore);
+
+    verify(service).getWorkflowDefinitionStatistics("dummy", createdAfter, createdBefore, modifiedAfter, modifiedBefore);
     assertThat(statistics.stateStatistics.size(), is(0));
   }
 }
