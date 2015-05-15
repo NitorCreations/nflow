57635991115e316383371e43a2a33aa362ebfcac
Juha Syrjälä <juha.syrjala@gmail.com>
2015-05-15 19:49:43 +0300

    Remove old and unused statistics code

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/workflow/definition/StateExecutionStatistics.java
+++
@@ -1,8 +0,0 @@
-package com.nitorcreations.nflow.engine.workflow.definition;
-
-public class StateExecutionStatistics {
-  public long executing;
-  public long sleeping;
-  public long queued;
-  public long nonScheduled;
-}
