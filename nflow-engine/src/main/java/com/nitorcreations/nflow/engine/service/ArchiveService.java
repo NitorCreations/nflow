51f6e0f8d089a8f651f6b69d58b8e0a4cb1cf535
Juha Syrjälä <juha.syrjala@gmail.com>
2015-07-10 20:15:43 +0300

    Add test for ArchiveService. Change archive service not to call archiving with empty list of archivable workflow ids.

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/service/ArchiveService.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/service/ArchiveService.java
@@ -7,6 +7,7 @@ import javax.inject.Named;
 
 import org.joda.time.DateTime;
 import com.nitorcreations.nflow.engine.internal.dao.ArchiveDao;
+import org.springframework.util.Assert;
 
 @Named
 public class ArchiveService {
@@ -14,10 +15,16 @@ public class ArchiveService {
   private ArchiveDao archiveDao;
 
   public int archiveWorkflows(DateTime olderThan, int batchSize) {
+    Assert.notNull(olderThan, "olderThan must not be null");
+    Assert.isTrue(batchSize > 0, "batchSize must be greater than 0");
+
     List<Integer> workflowIds;
     int archivedWorkflows = 0;
     do {
       workflowIds = archiveDao.listArchivableWorkflows(olderThan, batchSize);
+      if(workflowIds.isEmpty()) {
+        break;
+      }
       archiveDao.archiveWorkflows(workflowIds);
       archivedWorkflows += workflowIds.size();
     } while(!workflowIds.isEmpty());
