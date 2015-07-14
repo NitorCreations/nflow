e34f5105bb39adea8c4fb9b6c914020df43a4ea2
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2015-07-14 09:54:09 +0300

    format and update javadoc

--- nflow-engine/src/main/java/com/nitorcreations/nflow/engine/service/ArchiveService.java
+++ nflow-engine/src/main/java/com/nitorcreations/nflow/engine/service/ArchiveService.java
@@ -21,14 +21,14 @@ public class ArchiveService {
   private ArchiveDao archiveDao;
 
   /**
-   * Archive old and passive workflows. Copies workflow instance, workflow instance actions and state variables to
-   * corresponding archive tables and removes them production tables. Archives workflows that do not have
-   * <code>nextActivation</code> and whose modified time is earlier than <code>olderThan</code> parameter.
+   * Archive old (whose modified time is earlier than <code>olderThan</code> parameter) and passive (that do not have
+   * <code>nextActivation</code>) workflows. Copies workflow instances, workflow instance actions and state variables to
+   * corresponding archive tables and removes them from production tables.
    *
-   * @param olderThan
-   * @param batchSize number of workflow hierarchies to archive in single transactions. Typical value is 1-20. This
-   *                  parameter mostly affects on archival performance.
-   * @return number of archived workflows
+   * @param olderThan Passive workflow instances whose modified time is before this will be archived.
+   * @param batchSize Number of workflow hierarchies to archive in a single transaction. Typical value is 1-20. This parameter 
+   * mostly affects on archiving performance.
+   * @return Total number of archived workflows.
    */
   public int archiveWorkflows(DateTime olderThan, int batchSize) {
     Assert.notNull(olderThan, "olderThan must not be null");
@@ -47,9 +47,9 @@ public class ArchiveService {
       archiveDao.archiveWorkflows(workflowIds);
       archivedWorkflows += workflowIds.size();
 
-      double timeDiff = stopWatch.getTime()/1000.0;
-      log.debug("Archived {} workflows. {} workflows / second. Workflow ids: {}. ",
-              workflowIds.size(), archivedWorkflows / timeDiff, workflowIds);
+      double timeDiff = stopWatch.getTime() / 1000.0;
+      log.debug("Archived {} workflows. {} workflows / second. Workflow ids: {}. ", workflowIds.size(), archivedWorkflows
+          / timeDiff, workflowIds);
     } while (!workflowIds.isEmpty());
 
     log.info("Archiving finished. Archived {} workflows.", archivedWorkflows);
