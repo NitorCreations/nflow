5819ec0d4b73eb18efe3e53d685f532bce55664e
Juha Syrjälä <juha.syrjala@iki.fi>
2015-10-15 20:14:19 +0300

    Failing protractor tests fails now the build

--- Gruntfile.js
+++ Gruntfile.js
@@ -382,7 +382,7 @@ module.exports = function (grunt) {
     protractor: {
       options: {
         configFile: 'test/protractor.conf.js', // Default config file
-        keepAlive: true, // If false, the grunt process stops when the test fails.
+        keepAlive: false, // If false, the grunt process stops when the test fails.
         noColor: false, // If true, protractor will not use colors in its output.
         args: {
           // Arguments passed to the command
