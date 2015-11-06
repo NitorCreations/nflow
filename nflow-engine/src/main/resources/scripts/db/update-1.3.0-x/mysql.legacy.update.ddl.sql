3b752ec58fb3d24bff6c73fc62ba55c1e9f891f0
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2015-11-06 22:29:58 +0200

    remove duplicate statements

--- nflow-engine/src/main/resources/scripts/db/update-1.3.0-x/mysql.legacy.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-1.3.0-x/mysql.legacy.update.ddl.sql
@@ -6,8 +6,6 @@ alter table nflow workflow add external_next_activation timestamp default null;
 alter table nflow_workflow add constraint fk_workflow_parent
   foreign key (parent_workflow_id, parent_action_id) references nflow_workflow_action (workflow_id, id) on delete cascade;
 
-alter table nflow_executor modify host varchar(253) not null;
-
 alter table nflow_workflow add constraint fk_workflow_root
   foreign key (root_workflow_id) references nflow_workflow (id) on delete cascade;
 
