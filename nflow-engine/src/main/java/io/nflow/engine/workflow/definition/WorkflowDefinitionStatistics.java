0e911a143d4116475f6938e98e8cd45ae8859af6
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2016-07-15 14:44:44 +0300

    make WorkflowDefinitionsStatistics extend ModelObject

--- nflow-engine/src/main/java/io/nflow/engine/workflow/definition/WorkflowDefinitionStatistics.java
+++ nflow-engine/src/main/java/io/nflow/engine/workflow/definition/WorkflowDefinitionStatistics.java
@@ -1,9 +1,10 @@
 package io.nflow.engine.workflow.definition;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.nflow.engine.model.ModelObject;
 
 @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD", justification = "used by nflow-rest")
-public class WorkflowDefinitionStatistics {
+public class WorkflowDefinitionStatistics extends ModelObject {
   public long allInstances;
   public long queuedInstances;
 }
