7392585192023fbd5cd9be514d6c0243c920225d
Esa Puttonen <esa.puttonen@nitorcreations.com>
2017-03-15 21:01:30 +0200

    Fix connect.static issue experienced in Travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,8 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
+  var serveStatic = require('serve-static');
+
   // Configurable paths for the application
   var appConfig = {
     src: 'src',
@@ -76,12 +78,12 @@ module.exports = function (grunt) {
           open: true,
           middleware: function (connect) {
             return [
-              connect.static('.tmp'),
+              serveStatic('.tmp'),
               connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
+                serveStatic('./bower_components')
               ),
-              connect.static(appConfig.src)
+              serveStatic(appConfig.src)
             ];
           }
         }
@@ -91,13 +93,13 @@ module.exports = function (grunt) {
           port: 9001,
           middleware: function (connect) {
             return [
-              connect.static('.tmp'),
-              connect.static('test'),
+              serveStatic('.tmp'),
+              serveStatic('test'),
               connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
+                serveStatic('./bower_components')
               ),
-              connect.static(appConfig.src)
+              serveStatic(appConfig.src)
             ];
           }
         }
