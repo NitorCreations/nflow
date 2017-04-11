e4765984066f5dfd1cadf67987b1c1e741c0eba8
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2017-04-11 13:24:04 +0300

    set started in db migration scripts

--- nflow-engine/src/main/resources/scripts/db/update-4.1.0-x/postgresql.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-4.1.0-x/postgresql.update.ddl.sql
@@ -1,2 +1,5 @@
 alter table nflow_workflow add started timestamptz;
 alter table nflow_archive_workflow add started timestamptz;
+
+update table nflow_workflow set started = (select min(execution_start) from nflow_workflow_action where nflow_workflow_action.workflow_id = nflow_workflow.id)
+update table nflow_archive_workflow set started = (select min(execution_start) from nflow_archive_workflow_action where nflow_archive_workflow_action.workflow_id = nflow_workflow.id)
