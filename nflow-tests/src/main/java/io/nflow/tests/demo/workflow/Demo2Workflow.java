ba859c11d39563e754dcac7589aa431a9b257b05
Juha Syrjälä <juha.syrjala@iki.fi>
2019-03-07 06:34:04 +0200

    junit5: fix WorkflowDefinitionUpdateTest
    When running WorkflowDefinitionUpdateTest API returned
    unexpected workflow definitions.
    
    Spring component scan picked up a workflow that
    test was expecting not to be present. For some reason this didn't
    happen with H2 profile.
    
    Modify the test to use a new workflow which is not used
    by any other test and that is not component scanned.

---
+++ nflow-tests/src/main/java/io/nflow/tests/demo/workflow/Demo2Workflow.java
@@ -0,0 +1,48 @@
+package io.nflow.tests.demo.workflow;
+
+import io.nflow.engine.workflow.definition.*;
+import org.springframework.stereotype.Component;
+
+import static io.nflow.engine.workflow.definition.NextAction.moveToState;
+import static io.nflow.engine.workflow.definition.NextAction.stopInState;
+import static io.nflow.engine.workflow.definition.WorkflowStateType.*;
+
+public class Demo2Workflow extends WorkflowDefinition<Demo2Workflow.State> {
+
+  public static final String DEMO2_WORKFLOW_TYPE = "demo2";
+
+  public static enum State implements WorkflowState {
+    begin(start), process(normal), done(end), error(manual);
+
+    private WorkflowStateType type;
+
+    private State(WorkflowStateType type) {
+      this.type = type;
+    }
+
+    @Override
+    public WorkflowStateType getType() {
+      return type;
+    }
+
+    @Override
+    public String getDescription() {
+      return name();
+    }
+  }
+
+  public Demo2Workflow() {
+    super(DEMO2_WORKFLOW_TYPE, State.begin, State.error);
+    setDescription("Simple demo workflow: start -> process -> end");
+    permit(State.begin, State.process);
+    permit(State.process, State.done);
+  }
+
+  public NextAction begin(@SuppressWarnings("unused") StateExecution execution) {
+    return moveToState(State.process, "Go to process state");
+  }
+
+  public NextAction process(@SuppressWarnings("unused") StateExecution execution) {
+    return stopInState(State.done, "Go to done state");
+  }
+}
--- nflow-tests/src/test/java/io/nflow/tests/WorkflowDefinitionUpdateTest.java
+++ nflow-tests/src/test/java/io/nflow/tests/WorkflowDefinitionUpdateTest.java
@@ -1,9 +1,10 @@
 package io.nflow.tests;
 
+import static io.nflow.tests.demo.workflow.Demo2Workflow.DEMO2_WORKFLOW_TYPE;
 import static io.nflow.tests.demo.workflow.DemoWorkflow.DEMO_WORKFLOW_TYPE;
-import static io.nflow.tests.demo.workflow.StateWorkflow.STATE_WORKFLOW_TYPE;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import io.nflow.tests.demo.workflow.Demo2Workflow;
 import io.nflow.tests.extension.NflowServerConfig;
 import io.nflow.tests.extension.NflowServerExtension;
 import org.junit.jupiter.api.MethodOrderer;
@@ -15,14 +16,15 @@ import org.springframework.context.annotation.Bean;
 
 import io.nflow.rest.v1.msg.ListWorkflowDefinitionResponse;
 import io.nflow.tests.demo.workflow.DemoWorkflow;
-import io.nflow.tests.demo.workflow.StateWorkflow;
 
 @ExtendWith(NflowServerExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class WorkflowDefinitionUpdateTest extends AbstractNflowTest {
 
-  public static NflowServerConfig server = new NflowServerConfig.Builder().springContextClass(FirstConfiguration.class)
-      .prop("nflow.db.h2.url", "jdbc:h2:mem:workflowdefinitionupdatetest;TRACE_LEVEL_FILE=4;DB_CLOSE_DELAY=-1").build();
+  public static NflowServerConfig server = new NflowServerConfig.Builder()
+          .springContextClass(FirstConfiguration.class)
+          .prop("nflow.db.h2.url", "jdbc:h2:mem:workflowdefinitionupdatetest;TRACE_LEVEL_FILE=4;DB_CLOSE_DELAY=-1")
+          .build();
 
   public WorkflowDefinitionUpdateTest() {
     super(server);
@@ -37,8 +39,8 @@ public class WorkflowDefinitionUpdateTest extends AbstractNflowTest {
 
   static class SecondConfiguration {
     @Bean
-    public StateWorkflow stateWorkflow() {
-      return new StateWorkflow();
+    public Demo2Workflow demo2Workflow() {
+      return new Demo2Workflow();
     }
   }
 
@@ -47,7 +49,7 @@ public class WorkflowDefinitionUpdateTest extends AbstractNflowTest {
   public void demoWorkflowDefinitionIsReturned() {
     ListWorkflowDefinitionResponse[] definitions = getWorkflowDefinitions();
     assertWorkflowDefinitionExists(DEMO_WORKFLOW_TYPE, definitions, true);
-    assertWorkflowDefinitionExists(STATE_WORKFLOW_TYPE, definitions, false);
+    assertWorkflowDefinitionExists(DEMO2_WORKFLOW_TYPE, definitions, false);
   }
 
   @Test
@@ -72,7 +74,7 @@ public class WorkflowDefinitionUpdateTest extends AbstractNflowTest {
   public void bothDefinitionsAreReturned() {
     ListWorkflowDefinitionResponse[] definitions = getWorkflowDefinitions();
     assertWorkflowDefinitionExists(DEMO_WORKFLOW_TYPE, definitions, true);
-    assertWorkflowDefinitionExists(STATE_WORKFLOW_TYPE, definitions, true);
+    assertWorkflowDefinitionExists(DEMO2_WORKFLOW_TYPE, definitions, true);
   }
 
   private void assertWorkflowDefinitionExists(String type, ListWorkflowDefinitionResponse[] definitions, boolean shouldExist) {
