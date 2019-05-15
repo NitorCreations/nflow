a1710308b2744027ff20eeacf377d450da4a3219
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2019-05-15 11:31:54 +0300

    populate started timestamp in instance table based on actions

--- nflow-engine/src/main/resources/scripts/db/update-5.5.0-x/sqlserver.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-5.5.0-x/sqlserver.update.ddl.sql
@@ -1,1 +1,4 @@
 alter table nflow_workflow add started datetimeoffset(3);
+
+update nflow_workflow w, (select workflow_id, min(execution_start) as started from nflow_workflow_action group by workflow_id) a
+  set w.started = a.started where w.id = a.workflow_id
