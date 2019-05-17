16934e091621565af43646cd79c20d999529f46b
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2019-05-17 15:43:46 +0300

    restore WorkflowInstanceInclude.STARTED, mark as deprecated

--- nflow-engine/src/main/java/io/nflow/engine/service/WorkflowInstanceInclude.java
+++ nflow-engine/src/main/java/io/nflow/engine/service/WorkflowInstanceInclude.java
@@ -6,6 +6,13 @@ package io.nflow.engine.service;
 public enum WorkflowInstanceInclude {
 
   /**
+   * The execution start date of the first action of the workflow instance (WorkflowInstance.started).
+   * @deprecated This is not needed anymore, since the started timestamp is always read from the database when the instance is loaded.
+   */
+  @Deprecated
+  STARTED,
+
+  /**
    * The most recent values of all state variables (WorkflowInstance.stateVariables).
    */
   CURRENT_STATE_VARIABLES,
