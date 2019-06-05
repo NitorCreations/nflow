e2589737db94102522e3f2f9c6ede9ffff097d9b
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2019-06-05 14:11:34 +0300

    do not modify workflow instance modified timestamp when setting created timestamp

--- nflow-engine/src/main/resources/scripts/db/update-5.6.0-x/postgresql.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-5.6.0-x/postgresql.update.ddl.sql
@@ -2,6 +2,6 @@ alter table nflow_workflow add started timestamptz;
 alter table nflow_archive_workflow add started timestamptz;
 
 update nflow_workflow w, (select workflow_id, min(execution_start) as started from nflow_workflow_action group by workflow_id) a
-  set w.started = a.started where w.id = a.workflow_id;
+  set w.started = a.started, w.modified = w.modified where w.id = a.workflow_id;
 update nflow_archive_workflow w, (select workflow_id, min(execution_start) as started from nflow_archive_workflow_action group by workflow_id) a
-  set w.started = a.started where w.id = a.workflow_id;
+  set w.started = a.started, w.modified = w.modified where w.id = a.workflow_id;
