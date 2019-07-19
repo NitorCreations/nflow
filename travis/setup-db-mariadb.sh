aae29dd757ff0c1cc773e367a00aba861a30a76e
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2019-07-19 15:36:43 +0300

    Enable mariadb testing via docker

--- travis/setup-db-mariadb.sh
+++ travis/setup-db-mariadb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ev
 
-mysql -v -e "create database nflow character set utf8mb4;" -u root
-mysql -v -e "create user 'nflow'@'%' identified by 'nflow';" -u root
-mysql -v -e "create user 'nflow'@'localhost' identified by 'nflow';" -u root
-mysql -v -e "grant all on nflow.* TO 'nflow'@'%';" -u root
-mysql -v -e "grant all on nflow.* TO 'nflow'@'localhost';" -u root
-mysql -v -e "flush privileges;" -u root
+if [[ -n "$TRAVIS" ]]; then
+  sudo service mysql stop || true
+fi
+
+docker run --rm --name mariadb -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_DATABASE=nflow -e MYSQL_USER=nflow -e MYSQL_PASSWORD=nflow --publish 3306:3306 --detach mariadb:10.4 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+fgrep -m1 'ready for connections' <(timeout 120 docker logs -f mariadb 2>&1)
