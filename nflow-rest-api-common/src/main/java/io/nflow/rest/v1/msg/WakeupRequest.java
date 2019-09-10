449bacf681274e53939686ef7c523dcbaf23e9b5
Juha Syrjälä <juha.syrjala@iki.fi>
2019-09-10 10:19:34 +0300

    Init WakeupRequest.expectedStates with emptyList()

--- nflow-rest-api-common/src/main/java/io/nflow/rest/v1/msg/WakeupRequest.java
+++ nflow-rest-api-common/src/main/java/io/nflow/rest/v1/msg/WakeupRequest.java
@@ -1,6 +1,5 @@
 package io.nflow.rest.v1.msg;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -8,11 +7,13 @@ import io.nflow.engine.model.ModelObject;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import static java.util.Collections.emptyList;
+
 @ApiModel(description = "Request to wake up a sleeping workflow instance matching the given id if it is in one of the expected states.")
 @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD", justification = "jackson reads dto fields")
 public class WakeupRequest extends ModelObject {
 
   @ApiModelProperty("List of expected states. Can be empty, meaning any state.")
-  public List<String> expectedStates = new ArrayList<>();
+  public List<String> expectedStates = emptyList();
 
 }
