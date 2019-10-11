74d25ce6db983452cb316e21db7aa69157f1c79c
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2019-10-11 11:02:03 +0300

    add new indices to mssqlserver update script

--- nflow-engine/src/main/resources/scripts/db/update-5.7.0-x/sqlserver.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-5.7.0-x/sqlserver.update.ddl.sql
@@ -1,1 +1,21 @@
 create index nflow_workflow_polling on nflow_workflow(next_activation, status, executor_id, executor_group);
+
+create index nflow_workflow_parent on nflow_workflow(parent_workflow_id, parent_action_id);
+
+create index nflow_workflow_root on nflow_workflow(root_workflow_id);
+
+create index nflow_workflow_action_workflow on nflow_workflow_action(workflow_id);
+
+create index nflow_workflow_state_workflow on nflow_workflow_state(workflow_id);
+
+create index nflow_archive_workflow_activation on nflow_archive_workflow(next_activation, modified);
+
+create index nflow_archive_workflow_polling on nflow_archive_workflow(next_activation, status, executor_id, executor_group);
+
+create index nflow_archive_workflow_parent on nflow_archive_workflow(parent_workflow_id, parent_action_id);
+
+create index nflow_archive_workflow_root on nflow_archive_workflow(root_workflow_id);
+
+create index nflow_archive_workflow_action_workflow on nflow_archive_workflow_action(workflow_id);
+
+create index nflow_archive_workflow_state_workflow on nflow_archive_workflow_state(workflow_id);
