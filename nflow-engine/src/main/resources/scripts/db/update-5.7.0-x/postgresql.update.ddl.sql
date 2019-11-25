647cade3e079a1db81de27669f40ca5dc8b3e59f
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2019-11-25 08:40:19 +0200

    Added instrcutions on running priority addition in phases

--- nflow-engine/src/main/resources/scripts/db/update-5.7.0-x/postgresql.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-5.7.0-x/postgresql.update.ddl.sql
@@ -1,4 +1,21 @@
+-- For reasonably small nflow_workflow row count:
 alter table nflow_workflow add priority smallint not null default 0;
+-- For large nflow_workflow row count:
+--
+--   alter table nflow_workflow add priority smallint null;
+--
+-- followed by either:
+--
+--   update nflow_workflow set priority = 0 where priority is null;
+--
+-- or in batches of 100k with this query, repeated until no rows are affected:
+--
+--   update nflow_workflow set priority = 0 where priority is null limit 100000;
+--
+-- and finally:
+--
+--   alter table nflow_workflow alter column priority smallint not null default 0;
+
 alter table nflow_archive_workflow add priority smallint null;
 
 drop index if exists nflow_workflow_polling;
