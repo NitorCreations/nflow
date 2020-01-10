be5250c1bc1cf627c24d17eb847277c4458b8045
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-01-10 15:44:32 +0200

    remove unused import

--- nflow-engine/src/test/java/io/nflow/engine/internal/workflow/StateExecutionImplTest.java
+++ nflow-engine/src/test/java/io/nflow/engine/internal/workflow/StateExecutionImplTest.java
@@ -17,7 +17,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
