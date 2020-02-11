2b247de88886c4d5845508cc2424e3ff57de8ca0
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-02-11 12:54:09 +0200

    add javadoc for MaintenanceResults

--- nflow-engine/src/main/java/io/nflow/engine/service/MaintenanceResults.java
+++ nflow-engine/src/main/java/io/nflow/engine/service/MaintenanceResults.java
@@ -1,10 +1,28 @@
 package io.nflow.engine.service;
 
+/**
+ * Represent maintenance operation results.
+ */
 public class MaintenanceResults {
 
+  /**
+   * Number of workflows archived.
+   */
   public final int archivedWorkflows;
+
+  /**
+   * Number of workflows deleted from archive table.
+   */
   public final int deletedArchivedWorkflows;
+
+  /**
+   * Number of workflows deleted from main table.
+   */
   public final int deletedWorkflows;
+
+  /**
+   * Number of workflow states deleted.
+   */
   public final int deletedStates;
 
   MaintenanceResults(int archivedWorkflows, int deletedArchivedWorkflows, int deletedWorkflows, int deletedStates) {
@@ -14,6 +32,9 @@ public class MaintenanceResults {
     this.deletedStates = deletedStates;
   }
 
+  /**
+   * Build MaintenanceResults objects.
+   */
   public static class Builder {
 
     private int archivedWorkflows;
@@ -21,26 +42,59 @@ public class MaintenanceResults {
     private int deletedWorkflows;
     private int deletedStates;
 
+    /**
+     * Set number of workflows archived.
+     *
+     * @param archivedWorkflows
+     *          Number of workflows archived.
+     * @return this
+     */
     public Builder setArchivedWorkflows(int archivedWorkflows) {
       this.archivedWorkflows = archivedWorkflows;
       return this;
     }
 
+    /**
+     * Set number of archived workflows deleted.
+     *
+     * @param deletedArchivedWorkflows
+     *          Number of archived workflows deleted.
+     * @return this
+     */
     public Builder setDeletedArchivedWorkflows(int deletedArchivedWorkflows) {
       this.deletedArchivedWorkflows = deletedArchivedWorkflows;
       return this;
     }
 
+    /**
+     * Set number of workflows deleted.
+     *
+     * @param deletedWorkflows
+     *          Number of workflows deleted.
+     * @return this
+     */
     public Builder setDeletedWorkflows(int deletedWorkflows) {
       this.deletedWorkflows = deletedWorkflows;
       return this;
     }
 
+    /**
+     * Set number of workflow states deleted.
+     *
+     * @param deletedStates
+     *          Number of workflow states deleted.
+     * @return this
+     */
     public Builder setDeletedStates(int deletedStates) {
       this.deletedStates = deletedStates;
       return this;
     }
 
+    /**
+     * Build MaintenanceResults object.
+     *
+     * @return MaintenanceResults object.
+     */
     public MaintenanceResults build() {
       return new MaintenanceResults(archivedWorkflows, deletedArchivedWorkflows, deletedWorkflows, deletedStates);
     }
