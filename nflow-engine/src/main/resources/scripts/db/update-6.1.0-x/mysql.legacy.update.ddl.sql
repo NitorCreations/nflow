5943edf37f17dfd6816a735e0450b804f71bc171
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-02-11 14:52:57 +0200

    update update scripts

--- nflow-engine/src/main/resources/scripts/db/update-6.1.0-x/mysql.legacy.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-6.1.0-x/mysql.legacy.update.ddl.sql
@@ -1,11 +1,17 @@
+-- These can be run before nFlow is updated.
+
 alter table nflow_workflow drop constraint fk_workflow_parent;
 
 alter table nflow_workflow drop constraint fk_workflow_root;
   
+create index idx_workflow_parent on nflow_workflow(parent_workflow_id);
+
+create index nflow_workflow_action_workflow on nflow_workflow_action(workflow_id);
+
+create index idx_workflow_archive_parent on nflow_archive_workflow(parent_workflow_id);
+
+-- There must be run after nFlow is updated.
+
 alter table nflow_workflow drop column workflow_root_id;
 
 alter table nflow_archive_workflow drop column workflow_root_id;
-
-create index idx_workflow_parent on nflow_workflow (parent_workflow_id);
-
-create index idx_workflow_archive_parent on nflow_archive_workflow (parent_workflow_id);
--- nflow-engine/src/main/resources/scripts/db/update-6.1.0-x/sqlserver.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-6.1.0-x/sqlserver.update.ddl.sql
@@ -1,14 +1,19 @@
-if exists (select 1 from sys.indexes where name='nflow_archive_workflow_root')
-drop index nflow_archive_workflow_root;
+-- These can be run before nFlow is updated.
 
+if exists (select 1 from sys.indexes where name='fk_workflow_parent')
 alter table nflow_workflow drop constraint fk_workflow_parent;
 
+if exists (select 1 from sys.indexes where name='fk_workflow_root')
 alter table nflow_workflow drop constraint fk_workflow_root;
-  
+
+create index idx_workflow_parent on nflow_workflow(parent_workflow_id);
+
+create index nflow_workflow_action_workflow on nflow_workflow_action(workflow_id);
+
+create index idx_workflow_archive_parent on nflow_archive_workflow(parent_workflow_id);
+
+-- There must be run after nFlow is updated.
+
 alter table nflow_workflow drop column workflow_root_id;
 
 alter table nflow_archive_workflow drop column workflow_root_id;
-
-create index idx_workflow_parent on nflow_workflow (parent_workflow_id);
-
-create index idx_workflow_archive_parent on nflow_archive_workflow (parent_workflow_id);
