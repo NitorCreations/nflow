b4783f1c115b01fc600cfaca58b23e11dc8b62a4
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2020-02-20 13:43:16 +0200

    Move defaults back to properties

--- nflow-engine/src/main/java/io/nflow/engine/workflow/curated/MaintenanceWorkflowStarter.java
+++ nflow-engine/src/main/java/io/nflow/engine/workflow/curated/MaintenanceWorkflowStarter.java
@@ -1,40 +1,48 @@
 package io.nflow.engine.workflow.curated;
 
 import io.nflow.engine.service.MaintenanceConfiguration;
+import io.nflow.engine.service.MaintenanceConfiguration.ConfigurationItem;
 import io.nflow.engine.service.WorkflowInstanceService;
 import io.nflow.engine.workflow.instance.WorkflowInstance;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.Period;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Supplier;
+
 import static io.nflow.engine.workflow.curated.CronWorkflow.VAR_SCHEDULE;
 import static io.nflow.engine.workflow.curated.MaintenanceWorkflow.MAINTENANCE_WORKFLOW_TYPE;
 import static io.nflow.engine.workflow.curated.MaintenanceWorkflow.VAR_MAINTENANCE_CONFIGURATION;
-import static org.joda.time.Period.days;
-import static org.joda.time.Period.years;
+import static java.util.Optional.ofNullable;
 
 @Component
 public class MaintenanceWorkflowStarter {
   public static final String MAINTENANCE_WORKFLOW_DEFAULT_EXTERNAL_ID = "default";
 
-  protected final WorkflowInstanceService instanceService;
-  protected final boolean insertOnStartup;
+  protected WorkflowInstanceService instanceService;
+  protected boolean insertOnStartup;
+  protected String initialCronSchedule;
+  protected MaintenanceConfiguration initialConfiguration;
 
   public MaintenanceWorkflowStarter(Environment env, WorkflowInstanceService instanceService) {
     this.instanceService = instanceService;
     this.insertOnStartup = env.getRequiredProperty("nflow.maintenance.insertWorkflowIfMissing", Boolean.class);
+    this.initialCronSchedule = env.getRequiredProperty("nflow.maintenance.initial.cron");
+    MaintenanceConfiguration.Builder builder = new MaintenanceConfiguration.Builder();
+    apply(env, "archive", builder::withArchiveWorkflows);
+    apply(env, "delete", builder::withDeleteWorkflows);
+    apply(env, "deleteArchived", builder::withDeleteArchivedWorkflows);
+    this.initialConfiguration = builder.build();
   }
 
-  protected String getInitialCronSchedule() {
-    return "4 4 4 * * *";
-  }
-
-  protected MaintenanceConfiguration getInitialConfiguration() {
-    return new MaintenanceConfiguration.Builder()
-            .withDeleteArchivedWorkflows().setOlderThanPeriod(years(1)).done()
-            .withArchiveWorkflows().setOlderThanPeriod(days(45)).done()
-            .build();
+  private void apply(Environment env, String property, Supplier<ConfigurationItem.Builder> builderSupplier) {
+    ofNullable(env.getProperty("nflow.maintenance.initial." + property + ".olderThan"))
+            .map(StringUtils::trimToNull)
+            .map(Period::parse)
+            .ifPresent(period -> builderSupplier.get().setOlderThanPeriod(period).done());
   }
 
   @EventListener(ContextStartedEvent.class)
@@ -42,8 +50,8 @@ public class MaintenanceWorkflowStarter {
     if (insertOnStartup) {
       instanceService.insertWorkflowInstance(new WorkflowInstance.Builder()
               .setType(MAINTENANCE_WORKFLOW_TYPE)
-              .putStateVariable(VAR_SCHEDULE, getInitialCronSchedule())
-              .putStateVariable(VAR_MAINTENANCE_CONFIGURATION, getInitialConfiguration())
+              .putStateVariable(VAR_SCHEDULE, initialCronSchedule)
+              .putStateVariable(VAR_MAINTENANCE_CONFIGURATION, initialConfiguration)
               .setExternalId(MAINTENANCE_WORKFLOW_DEFAULT_EXTERNAL_ID)
               .build());
     }
