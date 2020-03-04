e17fb928b647c8d9ea80e7d20e66f950148b04aa
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-03-04 11:42:52 +0200

    fix typos in sql update script comments [ci skip]

--- nflow-engine/src/main/resources/scripts/db/update-6.2.0-x/sqlserver.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-6.2.0-x/sqlserver.update.ddl.sql
@@ -12,7 +12,7 @@ create index nflow_workflow_action_workflow on nflow_workflow_action(workflow_id
 
 create index idx_workflow_archive_parent on nflow_archive_workflow(parent_workflow_id);
 
--- There must be run after nFlow is updated.
+-- These must be run after nFlow is updated.
 
 alter table nflow_workflow drop column workflow_root_id;
 
