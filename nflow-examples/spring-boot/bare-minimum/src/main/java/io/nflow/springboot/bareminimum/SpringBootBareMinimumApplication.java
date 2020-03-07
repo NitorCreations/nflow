dc2263444626ef2fbfcafbf3591b9b3301f7af33
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-03-07 11:21:16 +0200

    sync spring-boot example apps with documentation

--- nflow-examples/spring-boot/bare-minimum/src/main/java/io/nflow/springboot/bareminimum/SpringBootBareMinimumApplication.java
+++ nflow-examples/spring-boot/bare-minimum/src/main/java/io/nflow/springboot/bareminimum/SpringBootBareMinimumApplication.java
@@ -1,15 +1,27 @@
 package io.nflow.springboot.bareminimum;
 
+import javax.inject.Inject;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 
 import io.nflow.engine.config.EngineConfiguration;
+import io.nflow.engine.service.WorkflowInstanceService;
+import io.nflow.engine.workflow.instance.WorkflowInstanceFactory;
 
 @SpringBootApplication
 @Import(EngineConfiguration.class)
 public class SpringBootBareMinimumApplication {
+
+  @Inject
+  private WorkflowInstanceService workflowInstances;
+
+  @Inject
+  private WorkflowInstanceFactory workflowInstanceFactory;
 
   @Bean
   public ExampleWorkflow exampleWorkflow() {
@@ -20,4 +32,12 @@ public class SpringBootBareMinimumApplication {
     SpringApplication.run(SpringBootBareMinimumApplication.class, args);
   }
 
+  @EventListener(ApplicationReadyEvent.class)
+  public void insertExampleWorkflow() {
+    workflowInstances.insertWorkflowInstance(workflowInstanceFactory.newWorkflowInstanceBuilder()
+        .setType(ExampleWorkflow.TYPE)
+        .setExternalId("example")
+        .putStateVariable(ExampleWorkflow.VAR_COUNTER, 0)
+        .build());
+  }
 }
--- nflow-examples/spring-boot/full-stack/src/main/java/io/nflow/springboot/fullstack/SpringBootFullStackApplication.java
+++ nflow-examples/spring-boot/full-stack/src/main/java/io/nflow/springboot/fullstack/SpringBootFullStackApplication.java
@@ -1,12 +1,18 @@
 package io.nflow.springboot.fullstack;
 
+import javax.inject.Inject;
+
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 
+import io.nflow.engine.service.WorkflowInstanceService;
+import io.nflow.engine.workflow.instance.WorkflowInstanceFactory;
 import io.nflow.rest.config.RestConfiguration;
 import io.nflow.rest.config.jaxrs.DateTimeParamConverterProvider;
 import io.nflow.rest.v1.jaxrs.ArchiveResource;
@@ -18,6 +24,12 @@ import io.nflow.rest.v1.jaxrs.WorkflowInstanceResource;
 @SpringBootApplication
 @Import(RestConfiguration.class)
 public class SpringBootFullStackApplication {
+
+  @Inject
+  private WorkflowInstanceService workflowInstances;
+
+  @Inject
+  private WorkflowInstanceFactory workflowInstanceFactory;
 
   @Bean
   public ExampleWorkflow exampleWorkflow() {
@@ -45,4 +57,12 @@ public class SpringBootFullStackApplication {
     SpringApplication.run(SpringBootFullStackApplication.class, args);
   }
 
+  @EventListener(ApplicationReadyEvent.class)
+  public void insertExampleWorkflow() {
+    workflowInstances.insertWorkflowInstance(workflowInstanceFactory.newWorkflowInstanceBuilder()
+        .setType(ExampleWorkflow.TYPE)
+        .setExternalId("example")
+        .putStateVariable(ExampleWorkflow.VAR_COUNTER, 0)
+        .build());
+  }
 }
