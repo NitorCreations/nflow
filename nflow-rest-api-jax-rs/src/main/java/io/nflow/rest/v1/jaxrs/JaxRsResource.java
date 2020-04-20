f6324e8c7b284a18174ba192e30bcd2f18b77ae3
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-04-20 21:31:02 +0300

    move common code to base class

--- nflow-rest-api-jax-rs/src/main/java/io/nflow/rest/v1/jaxrs/JaxRsResource.java
+++ nflow-rest-api-jax-rs/src/main/java/io/nflow/rest/v1/jaxrs/JaxRsResource.java
@@ -1,35 +1,21 @@
 package io.nflow.rest.v1.jaxrs;
 
 import static javax.ws.rs.core.Response.status;
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
-import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 import java.util.function.Supplier;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
 
-import io.nflow.engine.service.NflowNotFoundException;
 import io.nflow.rest.v1.ResourceBase;
-import io.nflow.rest.v1.msg.ErrorResponse;
 
 public class JaxRsResource extends ResourceBase {
 
   protected Response handleExceptions(Supplier<ResponseBuilder> responseBuilder) {
-    try {
-      return responseBuilder.get().build();
-    } catch (IllegalArgumentException e) {
-      return toErrorResponse(BAD_REQUEST, e.getMessage());
-    } catch (NflowNotFoundException e) {
-      return toErrorResponse(NOT_FOUND, e.getMessage());
-    } catch (Throwable t) {
-      return toErrorResponse(INTERNAL_SERVER_ERROR, t.getMessage());
-    }
+    return handleExceptions(() -> responseBuilder.get().build(), this::toErrorResponse);
   }
 
-  private Response toErrorResponse(Status status, String error) {
-    return status(status).entity(new ErrorResponse(error)).build();
+  private Response toErrorResponse(int statusCode, Object body) {
+    return status(statusCode).entity(body).build();
   }
 }
--- nflow-rest-api-spring-web/src/main/java/io/nflow/rest/v1/springweb/SpringWebResource.java
+++ nflow-rest-api-spring-web/src/main/java/io/nflow/rest/v1/springweb/SpringWebResource.java
@@ -1,34 +1,20 @@
 package io.nflow.rest.v1.springweb;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.ResponseEntity.status;
 
 import java.util.function.Supplier;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import io.nflow.engine.service.NflowNotFoundException;
 import io.nflow.rest.v1.ResourceBase;
-import io.nflow.rest.v1.msg.ErrorResponse;
 
 public abstract class SpringWebResource extends ResourceBase {
 
   protected ResponseEntity<?> handleExceptions(Supplier<ResponseEntity<?>> response) {
-    try {
-      return response.get();
-    } catch (IllegalArgumentException e) {
-      return toErrorResponse(BAD_REQUEST, e.getMessage());
-    } catch (NflowNotFoundException e) {
-      return toErrorResponse(NOT_FOUND, e.getMessage());
-    } catch (Throwable t) {
-      return toErrorResponse(INTERNAL_SERVER_ERROR, t.getMessage());
-    }
+    return handleExceptions(response::get, this::toErrorResponse);
   }
 
-  private ResponseEntity<?> toErrorResponse(HttpStatus status, String error) {
-    return status(status).body(new ErrorResponse(error));
+  private ResponseEntity<?> toErrorResponse(int statusCode, Object body) {
+    return status(statusCode).body(body);
   }
 }
