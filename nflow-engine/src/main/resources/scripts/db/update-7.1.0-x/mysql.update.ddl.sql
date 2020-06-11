730c290d47d0ee7e60308f62ee72661d0d895138
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-06-11 09:13:52 +0300

    fix mysql update scripts to use drop index instead of drop constraint

--- nflow-engine/src/main/resources/scripts/db/update-7.1.0-x/mysql.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.1.0-x/mysql.update.ddl.sql
@@ -1,4 +1,4 @@
-alter table nflow_archive_workflow drop constraint nflow_archive_workflow_uniq;
+alter table nflow_archive_workflow drop index nflow_archive_workflow_uniq;
 
 create index idx_workflow_archive_type on nflow_archive_workflow(type);
 
