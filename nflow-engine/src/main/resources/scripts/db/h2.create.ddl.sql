a3d2a4a87e05635e8d8653470d78771ea6e750cb
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-08-12 13:39:40 +0300

    add if not exists to create index statements for h2

--- nflow-engine/src/main/resources/scripts/db/h2.create.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/h2.create.ddl.sql
@@ -27,7 +27,7 @@ create unique index if not exists nflow_workflow_uniq on nflow_workflow (type, e
 
 create index if not exists nflow_workflow_polling on nflow_workflow(next_activation, status, executor_id, executor_group);
 
-create index idx_workflow_parent on nflow_workflow(parent_workflow_id);
+create index if not exists idx_workflow_parent on nflow_workflow(parent_workflow_id);
 
 create table if not exists nflow_workflow_action (
   id int not null auto_increment primary key,
@@ -42,7 +42,7 @@ create table if not exists nflow_workflow_action (
   constraint fk_action_workflow_id foreign key (workflow_id) references nflow_workflow(id)
 );
 
-create index nflow_workflow_action_workflow on nflow_workflow_action(workflow_id);
+create index if not exists nflow_workflow_action_workflow on nflow_workflow_action(workflow_id);
 
 create table if not exists nflow_workflow_state (
   workflow_id int not null,
@@ -102,8 +102,8 @@ create table if not exists nflow_archive_workflow (
   workflow_signal int
 );
 
-create index idx_workflow_archive_parent on nflow_archive_workflow(parent_workflow_id);
-create index idx_workflow_archive_type on nflow_archive_workflow(type);
+create index if not exists idx_workflow_archive_parent on nflow_archive_workflow(parent_workflow_id);
+create index if not exists idx_workflow_archive_type on nflow_archive_workflow(type);
 
 create table if not exists nflow_archive_workflow_action (
   id int not null primary key,
@@ -118,7 +118,7 @@ create table if not exists nflow_archive_workflow_action (
   constraint fk_arch_action_wf_id foreign key (workflow_id) references nflow_archive_workflow(id)
 );
 
-create index nflow_archive_workflow_action_workflow on nflow_archive_workflow_action(workflow_id);
+create index if not exists nflow_archive_workflow_action_workflow on nflow_archive_workflow_action(workflow_id);
 
 create table if not exists nflow_archive_workflow_state (
   workflow_id int not null,
