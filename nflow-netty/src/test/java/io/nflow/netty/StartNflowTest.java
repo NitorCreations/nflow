2fed84040ed25383811a78e6b85c2d2f2254f6fb
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2020-12-21 16:59:52 +0200

    Add smoke test for REST API

--- nflow-netty/src/test/java/io/nflow/netty/StartNflowTest.java
+++ nflow-netty/src/test/java/io/nflow/netty/StartNflowTest.java
@@ -1,5 +1,6 @@
 package io.nflow.netty;
 
+import static io.nflow.rest.v1.ResourcePaths.NFLOW_WORKFLOW_DEFINITION_PATH;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -10,6 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ApplicationContextEvent;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class StartNflowTest {
 
@@ -28,18 +36,29 @@ public class StartNflowTest {
         .registerSpringClasspathPropertySource("external.properties")
         .registerSpringApplicationListener(testListener);
     Map<String, Object> properties = new HashMap<>();
-    properties.put("nflow.db.create_on_startup", false);
+    String restApiPrefix = "nflow/api";
+    properties.put("nflow.db.create_on_startup", true);
     properties.put("nflow.autostart", false);
-    properties.put("nflow.autoinit", false);
+    properties.put("nflow.autoinit", true);
+    properties.put("nflow.rest.path.prefix", restApiPrefix);
+
     ApplicationContext ctx = startNflow.startNetty(7500, "local", "", properties);
 
     assertNotNull(testListener.applicationContextEvent);
     assertEquals("7500", ctx.getEnvironment().getProperty("port"));
     assertEquals("local", ctx.getEnvironment().getProperty("env"));
     assertEquals("externallyDefinedExecutorGroup", ctx.getEnvironment().getProperty("nflow.executor.group"));
-    assertEquals("false", ctx.getEnvironment().getProperty("nflow.db.create_on_startup"));
+    assertEquals("true", ctx.getEnvironment().getProperty("nflow.db.create_on_startup"));
     assertEquals("false", ctx.getEnvironment().getProperty("nflow.autostart"));
-    assertEquals("false", ctx.getEnvironment().getProperty("nflow.autoinit"));
+    assertEquals("true", ctx.getEnvironment().getProperty("nflow.autoinit"));
+
+    // Smoke test here also the netty REST API by fetching workflow definitions
+    WebClient client = WebClient.builder().baseUrl("http://localhost:7500")
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+    ClientResponse response = client.get().uri(restApiPrefix + NFLOW_WORKFLOW_DEFINITION_PATH).exchange().block();
+    assertEquals(HttpStatus.OK, response.statusCode());
+    JsonNode responseBody = response.bodyToMono(JsonNode.class).block();
+    assertEquals(responseBody.isArray(), true);
   }
 
 }
