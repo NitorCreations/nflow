baf7e77c87c47f01d0f7f2a1da065ff1d1930b62
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2020-12-22 20:03:27 +0200

    fix mysql upgrade scripts

--- nflow-engine/src/main/resources/scripts/db/update-7.2.0-x/mysql.legacy.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.2.0-x/mysql.legacy.update.ddl.sql
@@ -1,3 +1,3 @@
-alter table nflow_archive_workflow modify column retries int not null;
+alter table nflow_archive_workflow alter column retries drop default;
 
-alter table nflow_archive_workflow_action modify column executor_id int not null;
+alter table nflow_archive_workflow_action alter column executor_id drop default;
--- nflow-engine/src/main/resources/scripts/db/update-7.2.0-x/mysql.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.2.0-x/mysql.update.ddl.sql
@@ -1,6 +1,6 @@
-alter table nflow_archive_workflow modify column retries int not null;
-alter table nflow_archive_workflow modify column created timestamp(3);
-alter table nflow_archive_workflow modify column modified timestamp(3);
+alter table nflow_archive_workflow alter column retries drop default;
+alter table nflow_archive_workflow alter column created drop default;
+alter table nflow_archive_workflow alter column modified drop default;
 
-alter table nflow_archive_workflow_action modify column execution_start timestamp(3);
-alter table nflow_archive_workflow_action modify column execution_end timestamp(3);
+alter table nflow_archive_workflow_action alter column execution_start drop default;
+alter table nflow_archive_workflow_action alter column execution_end drop default;
