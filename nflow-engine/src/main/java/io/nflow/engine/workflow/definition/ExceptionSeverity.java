c0420cffbd8a5c223a337109f8698f956848ed03
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2021-02-01 17:23:59 +0200

    refactor

--- nflow-engine/src/main/java/io/nflow/engine/workflow/definition/ExceptionSeverity.java
+++ nflow-engine/src/main/java/io/nflow/engine/workflow/definition/ExceptionSeverity.java
@@ -1,5 +1,33 @@
 package io.nflow.engine.workflow.definition;
 
-public enum ExceptionSeverity {
-  ERROR, WARNING, INFO
+import static org.slf4j.event.Level.ERROR;
+
+import org.slf4j.event.Level;
+
+public class ExceptionSeverity {
+  public static final ExceptionSeverity DEFAULT = new ExceptionSeverity(ERROR, true);
+  public final Level logLevel;
+  public final boolean logStackTrace;
+
+  ExceptionSeverity(Level logLevel, boolean logStackTrace) {
+    this.logLevel = logLevel;
+    this.logStackTrace = logStackTrace;
+  }
+
+  public static class Builder {
+    private Level logLevel = ERROR;
+    private boolean logStackTrace = true;
+
+    public void setLogLevel(Level logLevel) {
+      this.logLevel = logLevel;
+    }
+
+    public void setLogStackTrace(boolean logStackTrace) {
+      this.logStackTrace = logStackTrace;
+    }
+
+    public ExceptionSeverity build() {
+      return new ExceptionSeverity(logLevel, logStackTrace);
+    }
+  }
 }
