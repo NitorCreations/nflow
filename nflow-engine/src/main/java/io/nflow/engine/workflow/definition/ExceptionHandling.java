bb6b560084773c5935ebbed9bc1cc2480cdd1ee4
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2021-02-01 23:49:54 +0200

    add javadocs

--- nflow-engine/src/main/java/io/nflow/engine/workflow/definition/ExceptionHandling.java
+++ nflow-engine/src/main/java/io/nflow/engine/workflow/definition/ExceptionHandling.java
@@ -4,9 +4,21 @@ import static org.slf4j.event.Level.ERROR;
 
 import org.slf4j.event.Level;
 
+/**
+ * Controls how an exception thrown by a state method should be handled by the workflow state processor.
+ */
 public class ExceptionHandling {
+  /**
+   * True when the state method processing should be retried.
+   */
   public final boolean isRetryable;
+  /**
+   * The log entry level for logging the exception.
+   */
   public final Level logLevel;
+  /**
+   * True when the exception stack trace of the exception should be logged. False to log only exception message.
+   */
   public final boolean logStackTrace;
 
   ExceptionHandling(boolean isRetryable, Level logLevel, boolean logStackTrace) {
@@ -15,26 +27,55 @@ public class ExceptionHandling {
     this.logStackTrace = logStackTrace;
   }
 
+  /**
+   * Builder for exception handling settings.
+   */
   public static class Builder {
     private boolean isRetryable = true;
     private Level logLevel = ERROR;
     private boolean logStackTrace = true;
 
+    /**
+     * Set if state method processing is retryable or not.
+     *
+     * @param isRetryable
+     *          True is state method processing should be retried.
+     * @return This.
+     */
     public Builder setRetryable(boolean isRetryable) {
       this.isRetryable = isRetryable;
       return this;
     }
 
+    /**
+     * Set the log entry level.
+     *
+     * @param logLevel
+     *          The log entry level.
+     * @return This.
+     */
     public Builder setLogLevel(Level logLevel) {
       this.logLevel = logLevel;
       return this;
     }
 
+    /**
+     * Set if exception stack trace should be logged or not.
+     *
+     * @param logStackTrace
+     *          True to log the exception stack trace, false to log the exception message only.
+     * @return This.
+     */
     public Builder setLogStackTrace(boolean logStackTrace) {
       this.logStackTrace = logStackTrace;
       return this;
     }
 
+    /**
+     * Create the exception handling object.
+     *
+     * @return Exception handling.
+     */
     public ExceptionHandling build() {
       return new ExceptionHandling(isRetryable, logLevel, logStackTrace);
     }
