7342a7aeb7500805d1bcb4473bd98a8b3003e52b
Esa Puttonen <esa.puttonen@nitorcreations.com>
2021-02-10 17:42:40 +0200

    Add maven bare-minimum example

---
+++ nflow-examples/spring-boot/bare-minimum/maven/src/test/java/io/nflow/springboot/bareminimum/maven/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nflow.springboot.bareminimum.maven.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
