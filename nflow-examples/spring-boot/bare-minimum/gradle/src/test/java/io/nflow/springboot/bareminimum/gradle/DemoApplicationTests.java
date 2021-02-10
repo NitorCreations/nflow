ec3c9b0eef94aa8de9c87206c950bdf230eec0d2
Esa Puttonen <esa.puttonen@nitorcreations.com>
2021-02-10 17:23:51 +0200

    Recreate and move Gradle bare-minimum example to dedicated directory

---
+++ nflow-examples/spring-boot/bare-minimum/gradle/src/test/java/io/nflow/springboot/bareminimum/gradle/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nflow.springboot.bareminimum.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
