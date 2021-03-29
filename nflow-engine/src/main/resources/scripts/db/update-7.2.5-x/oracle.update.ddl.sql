74665cad8585faea7bd72f6abdac36757bd90e96
Edvard Fonsell <edvard.fonsell@nitor.com>
2021-03-29 23:08:09 +0300

    drop and recreate nflow_workflow_action_id_seq

--- nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
@@ -4,7 +4,10 @@ alter sequence nflow_executor_id_seq nocache
 alter sequence nflow_workflow_id_seq nocache
 /
 
-alter sequence nflow_workflow_action_id_seq start with nflow_workflow_id_seq.nextval nocache
+drop sequence nflow_workflow_action_id_seq
+/
+
+create sequence nflow_workflow_action_id_seq start with (select max(id) + 1000 from nflow_workflow_action) nocache
 /
 
 create or replace trigger nflow_workflow_action_insert
