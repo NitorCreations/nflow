5f4cb0f46ae4c933e518ac60f17e5028478b6b09
Esa Puttonen <esa.puttonen@nitorcreations.com>
2021-03-30 19:41:52 +0300

    Tune Oracle SQL scripts

--- nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
@@ -4,10 +4,19 @@ alter sequence nflow_executor_id_seq nocache
 alter sequence nflow_workflow_id_seq nocache
 /
 
-drop sequence nflow_workflow_action_id_seq
-/
-
-create sequence nflow_workflow_action_id_seq start with (select max(id) + 1000 from nflow_workflow_action) nocache
+declare
+  id_with_slack number;
+begin
+  select max(id) + 1000 into id_with_slack from nflow_workflow_action;
+  if id_with_slack > 0 then
+    begin
+      execute immediate 'drop sequence nflow_workflow_action_id_seq';
+      exception when others then
+        null;
+    end;
+    execute immediate 'create sequence nflow_workflow_action_id_seq start with ' || id_with_slack || ' nocache';
+  end if;
+end;
 /
 
 create or replace trigger nflow_workflow_action_insert
