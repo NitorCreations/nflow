45b7af07d352dadd1d9d5694b259709031af5003
Esa Puttonen <esa.puttonen@nitorcreations.com>
2021-03-30 21:53:48 +0300

    Tune oracle update script still more

--- nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
+++ nflow-engine/src/main/resources/scripts/db/update-7.2.5-x/oracle.update.ddl.sql
@@ -4,18 +4,14 @@ alter sequence nflow_executor_id_seq nocache
 alter sequence nflow_workflow_id_seq nocache
 /
 
+drop sequence nflow_workflow_action_id_seq
+/
+
 declare
   id_with_slack number;
 begin
-  select max(id) + 1000 into id_with_slack from nflow_workflow_action;
-  if id_with_slack > 0 then
-    begin
-      execute immediate 'drop sequence nflow_workflow_action_id_seq';
-      exception when others then
-        null;
-    end;
-    execute immediate 'create sequence nflow_workflow_action_id_seq start with ' || id_with_slack || ' nocache';
-  end if;
+  select nvl(max(id),0) + 1000 into id_with_slack from nflow_workflow_action;
+  execute immediate 'create sequence nflow_workflow_action_id_seq start with ' || id_with_slack || ' nocache';
 end;
 /
 
