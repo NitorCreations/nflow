797e928c02f4ae05a7498c4e66df1e1a88ec8e1c
Juha Syrjälä <juha.syrjala@iki.fi>
2021-05-26 07:34:32 +0300

    SettingsTable uses DataTable

--- src/workflow-definition/SettingsTable.tsx
+++ src/workflow-definition/SettingsTable.tsx
@@ -1,27 +1,14 @@
 import { WorkflowDefinition } from "../types";
+import { DataTable } from "../component";
 
 const SettingsTable = (props: {definition: WorkflowDefinition}) => {
     const settings = props.definition.settings;
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Setting</th>
-                    <th>Value</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Object.keys(settings).map(key => (
-                    <tr key={key}>
-                        <td>{key}</td>
-                        <td><pre><code>
-                            {JSON.stringify(settings[key], null, 2)}
-                        </code></pre></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    )
+    const columns = [
+        {field: 'key', headerName: 'Setting'},
+        {field: 'value', headerName: 'Value', fieldRender: (value: any) => <pre><code>{value}</code></pre>},
+    ];
+    const rows: any[] = Object.keys(settings).map(key => ({key: key, value: JSON.stringify(settings[key], null, 2)}));
+    return <DataTable rows={rows} columns={columns} />;
 };
 
 export { SettingsTable };
