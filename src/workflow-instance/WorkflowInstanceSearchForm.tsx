0e635124cbbf7f3710ecb53f292fd0f9723d0d5c
Juha Syrjälä <juha.syrjala@iki.fi>
2021-05-26 11:51:45 +0300

    Use material ui in the forms

--- src/workflow-instance/WorkflowInstanceSearchForm.tsx
+++ src/workflow-instance/WorkflowInstanceSearchForm.tsx
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+    maxWidth: 500,
+  },
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const allMarker = '!-all-!';
 
 const statuses = [
-  "",
+  allMarker,
   "created",
   "inProgress",
   "finished",
@@ -11,26 +33,29 @@ const statuses = [
 ];
 
 const typeNames: any = {
-  "": "-- All workflow types --",
+  [allMarker]: "-- All workflow types --",
 };
 
 const stateNames: any = {
-  "": "-- All workflow states --",
+  [allMarker]: "-- All workflow states --",
 };
 
 const statusNames: any = {
-  "": "-- All statuses --",
+  [allMarker]: "-- All statuses --",
 };
 
 function WorkflowInstanceSearchForm(props: {
   definitions: Array<any>;
   onSubmit: (data: any) => any;
 }) {
+  const theme = useTheme();
+  const classes = useStyles();
+
   const queryParams = new URLSearchParams(useLocation().search);
   
-  const [type, setType] = useState<string>(queryParams.get("type") || "");
-  const [state, setState] = useState<string>(queryParams.get("state") || "");
-  const [status, setStatus] = useState<string>(queryParams.get("status") || "");
+  const [type, setType] = useState<string>(queryParams.get("type") || allMarker);
+  const [state, setState] = useState<string>(queryParams.get("state") || allMarker);
+  const [status, setStatus] = useState<string>(queryParams.get("status") || allMarker);
   const [businessKey, setBusinessKey] = useState<string>(
     queryParams.get("businessKey") || ""
   );
@@ -42,7 +67,7 @@ function WorkflowInstanceSearchForm(props: {
     queryParams.get("parentWorkflowId") || ""
   );
 
-  const types = [""].concat(props.definitions.map((d) => d.type));
+  const types = [allMarker].concat(props.definitions.map((d) => d.type));
 
   // TODO to lodash or not to lodash?
   const selectedWorkflow = props.definitions.filter((d) => d.type === type)[0];
@@ -50,7 +75,7 @@ function WorkflowInstanceSearchForm(props: {
     (selectedWorkflow && selectedWorkflow.states) ||
     []
   ).map((s: any) => s.id);
-  const states = [""].concat(selectedStates);
+  const states = [allMarker].concat(selectedStates);
 
   const handleSubmit = (e?: any) => {
     e && e.preventDefault();
@@ -66,6 +91,9 @@ function WorkflowInstanceSearchForm(props: {
     for (let field of Object.keys(data)) {
       if (data[field]) {
         data[field] = data[field].trim();
+      }
+      if (data[field] === allMarker) {
+        data[field] = undefined
       }
     }
     // remove empty fields, convert ids to numbers
@@ -93,91 +121,75 @@ function WorkflowInstanceSearchForm(props: {
   const setWorkflowType = (type: string) => {
     // when type is changed, need to reset all selections that depend on type
     setType(type);
-    setState("");
+    setState(allMarker);
   };
 
+  // TODO move to components
   const selection = (
+    label: string,
     items: Array<any>,
     selected: string,
     onChange: (v: string) => any,
     nameMap: any
   ) => {
     return (
-      <select value={selected} onChange={(e) => onChange(e.target.value)}>
+      <FormControl className={classes.formControl}>
+      <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={selected}
+        onChange={(e: any) => onChange(e.target.value)}
+      >
         {items.map((item) => (
-          <option key={item} value={item}>
+          <MenuItem key={item} value={item}>
             {nameMap[item] || item}
-          </option>
+          </MenuItem>
         ))}
-      </select>
+      </Select>
+    </FormControl>
     );
   };
 
   return (
-    <form>
+    <form className={classes.root}>
       <div>
-        <label>
-          Workflow type
-          {selection(types, type, setWorkflowType, typeNames)}
-        </label>
+          {selection('Workflow type', types, type, setWorkflowType, typeNames)}
+  
+          {selection('Workflow state', states, state, setState, stateNames)}
+
+          {selection('Workflow status', statuses, status, setStatus, statusNames)}
       </div>
 
       <div>
-        <label>
-          Workflow state
-          {selection(states, state, setState, stateNames)}
-        </label>
+        <TextField
+          label="Business key"
+          value={businessKey}
+          onChange={(e) => setBusinessKey(e.target.value)}
+        />
+
+        <TextField
+          label="External id"
+          value={externalId}
+          onChange={(e) => setExternalId(e.target.value)}
+        />
+
+        <TextField
+          label="Workflow id"
+          type="number"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+
+      <TextField
+          label="Workflow parent id"
+          type="number"
+          value={parentInstanceId}
+          onChange={(e) => setParentInstanceId(e.target.value)}
+        />
       </div>
 
-      <div>
-        <label>
-          Workflow status
-          {selection(statuses, status, setStatus, statusNames)}
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Business key
-          <input
-            value={businessKey}
-            onChange={(e) => setBusinessKey(e.target.value)}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          External id
-          <input
-            value={externalId}
-            onChange={(e) => setExternalId(e.target.value)}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Workflow instance id
-          <input
-            type="number"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Workflow parent instance id
-          <input
-            type="number"
-            value={parentInstanceId}
-            onChange={(e) => setParentInstanceId(e.target.value)}
-          />
-        </label>
-      </div>
-      <button onClick={handleSubmit}>Search</button>
+      <Button onClick={handleSubmit} variant="contained">Search</Button>
     </form>
   );
 }
