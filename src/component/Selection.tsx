e6c29a15d82b0bfaab3eee60b38e33dd5a1c2e16
Juha Syrjälä <juha.syrjala@iki.fi>
2021-05-27 12:48:11 +0300

    Add Selection component

---
+++ src/component/Selection.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+
+let index = 0;
+function Selection(props: {
+    label: string,
+    items: string[],
+    selected: string,
+    onChange: (v: string) => any,
+    getSelectionLabel: (v: string) => any,
+}) {
+    // TODO https://github.com/mui-org/material-ui/issues/13394
+    //      will log ugly warning in dev first time the popup is opened
+    //      Material-UI v5 should fix this. Does not appear in prod.
+    let currentIndex = ++index;
+    return (
+        <FormControl>
+            <InputLabel id={"select-label-" + currentIndex}>{props.label}</InputLabel>
+            <Select
+                labelId={"select-label-" + currentIndex}
+                id={"selection-" + currentIndex}
+                value={props.selected}
+                onChange={(e: any) => props.onChange(e.target.value)}
+            >
+                {props.items.map((item) => (
+                    <MenuItem key={item} value={item}>
+                        {props.getSelectionLabel(item)}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
+
+export { Selection };
