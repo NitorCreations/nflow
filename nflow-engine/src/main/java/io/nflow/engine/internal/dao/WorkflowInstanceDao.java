cc7c6db20536676a879f00935c1dfb7197d095c2
SaurabhGarg1 <s.garg1@outlook.com>
2021-09-10 14:21:45 +0300

    Use workflowId to query the results in nflow_workflow_state (#458)
    * Use workflowId to query the results in nflow_workflow_state
    
    using workflowId in the query will yield into faster response, because now it searches through the all actionId regardless of the workflowId which takes time.
    
    * Fix problem spotted by tests
    
    * Update Release notes

--- nflow-engine/src/main/java/io/nflow/engine/internal/dao/WorkflowInstanceDao.java
+++ nflow-engine/src/main/java/io/nflow/engine/internal/dao/WorkflowInstanceDao.java
@@ -736,13 +736,14 @@ public class WorkflowInstanceDao {
     String sql;
     if (actions < maxActions) {
       sql = "select * from nflow_workflow_state where workflow_id = ? order by action_id, state_key asc";
+      return jdbc.query(sql, new WorkflowActionStateRowMapper(), instance.id);
     } else {
       sql = "select nflow_workflow_state.* from ("
           + sqlVariants.limit("select id from nflow_workflow_action where workflow_id = ? order by id desc", maxActions)
-          + ") action_id inner join nflow_workflow_state on action_id.id = nflow_workflow_state.action_id "
+          + ") action_id inner join nflow_workflow_state on nflow_workflow_state.workflow_id = ? and action_id.id = nflow_workflow_state.action_id "
           + "order by nflow_workflow_state.action_id, nflow_workflow_state.state_key asc";
+      return jdbc.query(sql, new WorkflowActionStateRowMapper(), instance.id, instance.id);
     }
-    return jdbc.query(sql, new WorkflowActionStateRowMapper(), instance.id);
   }
 
   @Transactional(propagation = MANDATORY)
