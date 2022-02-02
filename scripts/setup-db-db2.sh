d3604c35c65b9664a3d1a50ec0841f852b0fac9b
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2022-02-02 19:09:30 +0200

    Enable sqlserver tests on github actions (#491)
    * Test with sqlserver docker
    * Wait until sqlserver is ready
    * Try exporting junit test results
    * Pass DB_VERSION=old/latest from github action to db set scripts

--- scripts/setup-db-db2.sh
+++ scripts/setup-db-db2.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -ev
 
-VER=11.5.0.0a
+DB_VERSION=${DB_VERSION:-latest}
+case $DB_VERSION in
+  old)
+    DB_VERSION=11.5.0.0a
+    ;;
+  latest)
+    DB_VERSION=11.5.0.0a
+    ;;
+esac
 
-docker run --rm --name db2 --entrypoint /bin/sh ibmcom/db2:$VER -c "cat /opt/ibm/db2/V*/java/db2jcc4.jar" > db2jcc4.jar
+docker run --pull=always --rm --name db2 --entrypoint /bin/sh ibmcom/db2:$DB_VERSION -c "cat /opt/ibm/db2/V*/java/db2jcc4.jar" > db2jcc4.jar
 
-docker run --rm --name db2 --cap-add IPC_LOCK --cap-add IPC_OWNER -e 'instance_name=root' -e 'DB2INST1_PASSWORD=nflow' -e 'LICENSE=accept' -e 'DBNAME=nflow' --publish 50000:50000 --detach ibmcom/db2:$VER db2start
+docker run --rm --name db2 --cap-add IPC_LOCK --cap-add IPC_OWNER -e 'instance_name=root' -e 'DB2INST1_PASSWORD=nflow' -e 'LICENSE=accept' -e 'DBNAME=nflow' --publish 50000:50000 --detach ibmcom/db2:$DB_VERSION db2start
 
-echo "waiting for DB2 to start"
 fgrep -m1 'Setup has completed' <(timeout 240 docker logs -f db2)
-echo "DB2 started"
 
 #docker exec -it db2 su - db2inst1 -c '/opt/ibm/db2/V*/bin/db2 -tvs "CREATE DATABASE nflow USING CODESET UTF-8 TERRITORY us;"'
 #docker exec -it db2 su - db2inst1 -c '/opt/ibm/db2/V*/bin/db2 -tvs "ACTIVATE DATABASE nflow;"'
--- scripts/setup-db-mariadb.sh
+++ scripts/setup-db-mariadb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -ev
 
-if [[ -n "$TRAVIS" ]]; then
-  sudo service mysql stop || true
-fi
+DB_VERSION=${DB_VERSION:-latest}
+case $DB_VERSION in
+  old)
+    DB_VERSION=10.2 # supported until may/2022
+    ;;
+  latest)
+    DB_VERSION=10.7
+    ;;
+esac
 
-VER=10.7
-if [[ "$1" == 11 ]]; then
-  VER=10.2 # supported until may/2022
-fi
-
-docker run --rm --name mariadb -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_DATABASE=nflow -e MYSQL_USER=nflow -e MYSQL_PASSWORD=nflow --publish 3306:3306 --detach mariadb:$VER --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --pull=always --rm --name mariadb -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_DATABASE=nflow -e MYSQL_USER=nflow -e MYSQL_PASSWORD=nflow --publish 3306:3306 --detach mariadb:$DB_VERSION --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 fgrep -m1 'ready for connections' <(timeout 120 docker logs -f mariadb 2>&1)
