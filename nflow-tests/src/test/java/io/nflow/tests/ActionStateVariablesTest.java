a5343dc50e1a58b56fdf028d373111c705154ab1
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2022-07-19 11:14:12 +0300

    Fix test code that failed with nullpointer, but no logic changes so the assert might still fail randomly

--- nflow-tests/src/test/java/io/nflow/tests/ActionStateVariablesTest.java
+++ nflow-tests/src/test/java/io/nflow/tests/ActionStateVariablesTest.java
@@ -2,13 +2,14 @@ package io.nflow.tests;
 
 import static io.nflow.rest.v1.ApiWorkflowInstanceInclude.actionStateVariables;
 import static io.nflow.rest.v1.ApiWorkflowInstanceInclude.actions;
+import static io.nflow.tests.demo.workflow.ActionStateVariableWorkflow.MAX_STATE_VAR_VALUE;
 import static java.lang.Thread.sleep;
 import static java.time.Duration.ofSeconds;
 import static java.util.UUID.randomUUID;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import org.junit.jupiter.api.MethodOrderer;
@@ -17,7 +18,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.context.annotation.Bean;
 
-import io.nflow.rest.v1.msg.Action;
 import io.nflow.rest.v1.msg.CreateWorkflowInstanceRequest;
 import io.nflow.rest.v1.msg.CreateWorkflowInstanceResponse;
 import io.nflow.rest.v1.msg.ListWorkflowInstanceResponse;
@@ -56,8 +56,8 @@ public class ActionStateVariablesTest extends AbstractNflowTest {
   @Order(2)
   public void checkActionStateVariables() {
     int maxActions = 5;
-    ListWorkflowInstanceResponse response = assertTimeoutPreemptively(ofSeconds(5), () -> {
-      ListWorkflowInstanceResponse wf = null;
+    var response = assertTimeoutPreemptively(ofSeconds(5), () -> {
+      ListWorkflowInstanceResponse wf;
       do {
         sleep(200);
         wf = getInstanceIdResource(createResponse.id)
@@ -71,8 +71,8 @@ public class ActionStateVariablesTest extends AbstractNflowTest {
 
     assertThat(response.actions, hasSize(maxActions));
     for (int i = 0; i < maxActions; i++) {
-      Action action = response.actions.get(i);
-      assertEquals(ActionStateVariableWorkflow.MAX_STATE_VAR_VALUE - i, action.updatedStateVariables.get("stateVar"));
+      var action = response.actions.get(i);
+      assertThat(action.updatedStateVariables, hasEntry("stateVar", MAX_STATE_VAR_VALUE - i));
     }
   }
 }
