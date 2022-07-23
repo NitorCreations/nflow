d0d694dbee12eb8054ca0b217f869564ff78b656
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2022-07-23 16:24:10 +0300

    DB2 seems to have a jdbc driver in maven repository

--- scripts/setup-db-db2.sh
+++ scripts/setup-db-db2.sh
@@ -14,9 +14,7 @@ case $DB_VERSION in
     ;;
 esac
 
-$tool run --pull=always --rm --name db2 --entrypoint /bin/sh ibmcom/db2:$DB_VERSION -c "cat /opt/ibm/db2/V*/java/db2jcc4.jar" > db2jcc4.jar
-
-$tool run --rm --name db2 --cap-add IPC_LOCK --cap-add IPC_OWNER -e 'instance_name=root' -e 'DB2INST1_PASSWORD=nflow' -e 'LICENSE=accept' -e 'DBNAME=nflow' --publish 50000:50000 --detach ibmcom/db2:$DB_VERSION db2start
+$tool run --rm --name db2 --cap-add IPC_LOCK --cap-add IPC_OWNER -e instance_name=root -e DB2INST1_PASSWORD=nflow -e LICENSE=accept -e DBNAME=nflow -e ARCHIVE_LOGS=false --publish 50000:50000 --detach ibmcom/db2:$DB_VERSION
 
 fgrep -m1 'Setup has completed' <(timeout 240 $tool logs -f db2)
 
