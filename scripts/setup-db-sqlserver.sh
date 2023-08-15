7d588724952cc5c71c01f2c63593df4545b5451c
Tuomo Ala-Vannesluoma <ext-tuomo.ala-vannesluoma@elisa.fi>
2023-08-15 11:20:12 +0300

    Fix sqlserver password env variable and make stronger

--- scripts/setup-db-sqlserver.sh
+++ scripts/setup-db-sqlserver.sh
@@ -14,13 +14,13 @@ case $DB_VERSION in
     ;;
 esac
 
-$tool run --pull=always  --rm --name mssql -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passWord1%' --publish 1433:1433 --detach mcr.microsoft.com/mssql/server:$DB_VERSION
+$tool run --pull=always  --rm --name mssql -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=passWord1%' --publish 1433:1433 --detach mcr.microsoft.com/mssql/server:$DB_VERSION
 
 fgrep -m1 'Recovery is complete' <(timeout 240 $tool logs -f mssql 2>&1)
 
 sqlcmd="$tool exec -t mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P passWord1% -e -x"
 $sqlcmd -Q "create database nflow"
-$sqlcmd -d nflow -Q "create login [nflow] with password='nFlow42%', default_database=[nflow]"
+$sqlcmd -d nflow -Q "create login [nflow] with password='FC8%1knw', default_database=[nflow]"
 $sqlcmd -d nflow -Q "create user [nflow] for login [nflow] with default_schema=[nflow]"
 $sqlcmd -d nflow -Q "create schema nflow authorization nflow"
 $sqlcmd -d nflow -Q "grant connect to [nflow]"
