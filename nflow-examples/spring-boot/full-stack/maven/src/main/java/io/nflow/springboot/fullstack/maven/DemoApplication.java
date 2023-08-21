1644b29b56cdc12115ca91015b00f27543ffdfe7
Tuomo Ala-Vannesluoma <ext-tuomo.ala-vannesluoma@elisa.fi>
2023-08-21 16:13:40 +0300

    Fix ObjectMapper issues as RestConfiguration bean will conflict with engine bean and can't be imported

--- nflow-examples/spring-boot/full-stack/maven/src/main/java/io/nflow/springboot/fullstack/maven/DemoApplication.java
+++ nflow-examples/spring-boot/full-stack/maven/src/main/java/io/nflow/springboot/fullstack/maven/DemoApplication.java
@@ -1,20 +1,33 @@
 package io.nflow.springboot.fullstack.maven;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.nflow.engine.config.EngineConfiguration;
+import io.nflow.engine.config.NFlow;
+import io.nflow.rest.config.NflowRestApiPropertiesConfiguration;
+import io.nflow.rest.config.RestConfiguration;
 import jakarta.inject.Inject;
 
+import jakarta.inject.Named;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.EventListener;
 
-import io.nflow.rest.config.RestConfiguration;
 import io.nflow.engine.service.WorkflowInstanceService;
 import io.nflow.engine.workflow.instance.WorkflowInstanceFactory;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_TRAILING_TOKENS;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static io.nflow.rest.config.RestConfiguration.REST_OBJECT_MAPPER;
+
 @SpringBootApplication
-@Import(RestConfiguration.class)
+@Import({ EngineConfiguration.class, NflowRestApiPropertiesConfiguration.class })
+@ComponentScan(value = "io.nflow.rest", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RestConfiguration.class))
 public class DemoApplication {
 
   @Inject
@@ -30,6 +43,16 @@ public class DemoApplication {
         .setExternalId("example")
         .putStateVariable(ExampleWorkflow.VAR_COUNTER, 0)
         .build());
+  }
+
+  @Primary
+  @Bean
+  @Named(REST_OBJECT_MAPPER)
+  public ObjectMapper nflowRestObjectMapper(@NFlow ObjectMapper nflowObjectMapper) {
+    ObjectMapper restObjectMapper = nflowObjectMapper.copy();
+    restObjectMapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    restObjectMapper.enable(FAIL_ON_TRAILING_TOKENS);
+    return restObjectMapper;
   }
 
   @Bean
