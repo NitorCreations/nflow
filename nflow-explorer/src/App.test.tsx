b0dbaeb3ff1faf7076a479830213259642bd6bf9
Edvard Fonsell <edvard.fonsell@nitor.com>
2023-08-21 15:00:15 +0300

    rename nflow-explorer-ng to nflow-explorer

--- nflow-explorer/src/App.test.tsx
+++ nflow-explorer/src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import App from './App';
 
-test("renders navi", () => {
+test('renders navi', () => {
   render(<App />);
   const logoElement = screen.getByAltText(/nFlow-logo/i);
   expect(logoElement).toBeInTheDocument();
---
+++ nflow-explorer/src/cache.ts
@@ -0,0 +1,28 @@
+class Cache<T> {
+  private cache = new Map<string, {ts: number; value: T}>();
+  private maxAgeMillis: number;
+
+  constructor(maxAgeMillis: number) {
+    this.maxAgeMillis = maxAgeMillis;
+  }
+
+  public setAndReturn(key: string, value: T): T {
+    const now = new Date().getTime();
+    this.cache.set(key, {ts: now, value: value});
+    return value;
+  }
+
+  public get(key: string): T | undefined {
+    const now = new Date().getTime();
+    const entry = this.cache.get(key);
+    if (!entry) {
+      return undefined;
+    }
+    if (now - entry.ts < this.maxAgeMillis) {
+      return entry.value;
+    }
+    return undefined;
+  }
+}
+
+export {Cache};
